8b4eb7b7d80182ae4f107894ab22f2d80668a3a5
Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
2022-06-02 10:56:35 -0700

    Changed buckets for 20 minutes
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>

--- pkg/isb/forward/metrics.go
+++ pkg/isb/forward/metrics.go
@@ -65,8 +65,8 @@ var platformError = promauto.NewCounterVec(prometheus.CounterOpts{
 var forwardAChunkProcessingTime = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Subsystem: "forwarder",
 	Name:      "forward_chunk_processing_time",
-	Help:      "Processing times of the entire forward a chunk (1 millisecond to 30 minutes)",
-	Buckets:   prometheus.ExponentialBucketsRange(1000, 1000000*1800, 60),
+	Help:      "Processing times of the entire forward a chunk (100 microseconds to 20 minutes)",
+	Buckets:   prometheus.ExponentialBucketsRange(100, 60000000*20, 60),
 }, []string{"vertex", "pipeline", "from", "to"})
 
 // udfProcessingTime is a histogram to Observe UDF Processing times as a whole
@@ -74,15 +74,15 @@ var udfProcessingTime = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Subsystem: "forwarder",
 	Name:      "udf_processing_time",
 	Help:      "Processing times of UDF (100 microseconds to 15 minutes)",
-	Buckets:   prometheus.ExponentialBucketsRange(100, 1000000*900, 60),
+	Buckets:   prometheus.ExponentialBucketsRange(100, 60000000*15, 60),
 }, []string{"vertex", "pipeline", "buffer"})
 
 // concurrentUDFProcessingTime is a histogram to Observe UDF Processing times as a whole
 var concurrentUDFProcessingTime = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Subsystem: "forwarder",
 	Name:      "concurrent_udf_processing_time",
-	Help:      "Processing times of Concurrent UDF (1 millisecond to 30 minutes)",
-	Buckets:   prometheus.ExponentialBucketsRange(1000, 1000000*1800, 60),
+	Help:      "Processing times of Concurrent UDF (100 microseconds to 20 minutes)",
+	Buckets:   prometheus.ExponentialBucketsRange(100, 60000000*20, 60),
 }, []string{"vertex", "pipeline", "buffer"})
 
 // udfReadMessagesCount is used to indicate the number of messages read by UDF
