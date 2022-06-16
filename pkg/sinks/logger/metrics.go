0d692e5a92c289b5a1394cd254d01e9f9f9e9d4d
Krithika3 <krithika.vkumar@gmail.com>
2022-06-16 09:10:55 -0700

    docs: Document for all the different metrics we provide (#55)
    * Added metrics doc
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>

--- pkg/sinks/logger/metrics.go
+++ pkg/sinks/logger/metrics.go
@@ -5,8 +5,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-// logSinkErrors is used to indicate the number of messages written to log sink
-var logSinkReadCount = promauto.NewCounterVec(prometheus.CounterOpts{
+// logSinkWriteCount is used to indicate the number of messages written to log sink
+var logSinkWriteCount = promauto.NewCounterVec(prometheus.CounterOpts{
 	Subsystem: "log_sink",
 	Name:      "write_total",
 	Help:      "Total number of messages written to log sink",
