5ab5f44a7321370d56201177af58548efe15977d
Derek Wang <whynowy@gmail.com>
2022-06-22 16:13:28 -0700

    test: e2e test for filtering builtin function (#68)
    * test: more e2e test cases
    
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- test/e2e/functional_test.go
+++ test/e2e/functional_test.go
@@ -4,6 +4,7 @@ package e2e
 
 import (
 	"testing"
+	"time"
 
 	dfv1 "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
 	. "github.com/numaproj/numaflow/test/fixtures"
@@ -29,10 +30,10 @@ func (s *FunctionalSuite) TestCreateSimplePipeline() {
 
 	w.Expect().
 		VertexPodsRunning().
-		VertexPodLogContains("input", "main", LogSourceVertexStarted).
-		VertexPodLogContains("p1", "main", LogUDFVertexStarted).
-		VertexPodLogContains("output", "main", LogSinkVertexStarted).
-		VertexPodLogContains("output", "main", `"Data":".*","Createdts":.*`)
+		VertexPodLogContains("input", LogSourceVertexStarted).
+		VertexPodLogContains("p1", LogUDFVertexStarted, PodLogCheckOptionWithContainer("main")).
+		VertexPodLogContains("output", LogSinkVertexStarted).
+		VertexPodLogContains("output", `"Data":".*","Createdts":.*`)
 
 	defer w.VertexPodPortForward("input", 8001, dfv1.VertexMetricsPort).
 		VertexPodPortForward("p1", 8002, dfv1.VertexMetricsPort).
@@ -56,6 +57,42 @@ func (s *FunctionalSuite) TestCreateSimplePipeline() {
 		Status(200).
 		Body().
 		Contains("total")
+}
+
+func (s *FunctionalSuite) TestFiltering() {
+	w := s.Given().Pipeline("@testdata/filtering.yaml").
+		When().
+		CreatePipelineAndWait()
+
+	defer w.DeletePipelineAndWait()
+
+	w.Expect().
+		VertexPodsRunning().
+		VertexPodLogContains("in", LogSourceVertexStarted).
+		VertexPodLogContains("p1", LogUDFVertexStarted, PodLogCheckOptionWithContainer("main")).
+		VertexPodLogContains("out", LogSinkVertexStarted)
+
+	defer w.VertexPodPortForward("in", 8443, dfv1.VertexHTTPSPort).
+		TerminateAllPodPortForwards()
+
+	HTTPExpect(s.T(), "https://localhost:8443").POST("/vertices/in").WithBytes([]byte(`{"id": 180, "msg": "hello", "expect0": "fail", "desc": "A bad example"}`)).
+		Expect().
+		Status(204)
+
+	HTTPExpect(s.T(), "https://localhost:8443").POST("/vertices/in").WithBytes([]byte(`{"id": 80, "msg": "hello1", "expect1": "fail", "desc": "A bad example"}`)).
+		Expect().
+		Status(204)
+
+	HTTPExpect(s.T(), "https://localhost:8443").POST("/vertices/in").WithBytes([]byte(`{"id": 80, "msg": "hello", "expect2": "fail", "desc": "A bad example"}`)).
+		Expect().
+		Status(204)
+
+	HTTPExpect(s.T(), "https://localhost:8443").POST("/vertices/in").WithBytes([]byte(`{"id": 80, "msg": "hello", "expect3": "succeed", "desc": "A good example"}`)).
+		Expect().
+		Status(204)
+
+	w.Expect().VertexPodLogContains("out", "expect3")
+	w.Expect().VertexPodLogNotContains("out", "expect[0-2]", PodLogCheckOptionWithTimeout(2*time.Second))
 }
 
 func TestFunctionalSuite(t *testing.T) {
