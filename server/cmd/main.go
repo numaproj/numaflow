4b1f40508d65ddf5f731af20780ee04f177ad2cc
Krithika3 <krithika.vkumar@gmail.com>
2022-06-22 13:54:50 -0700

    feat: Merge UX server code (#67)
    * Moved the api server folder to numaflow
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * Skipping routes test
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    Co-authored-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

---
+++ server/cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+	"strings"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+
+	sharedtls "github.com/numaproj/numaflow/pkg/shared/tls"
+	"github.com/numaproj/numaflow/server/routes"
+)
+
+var (
+	rewritePathPrefixes = []string{
+		"/namespaces",
+	}
+)
+
+func main() {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.RedirectTrailingSlash = true
+	router.Use(static.Serve("/", static.LocalFile("./ui/build", true)))
+	routes.Routes(router)
+	router.Use(UrlRewrite(router))
+	cert, err := sharedtls.GenerateX509KeyPair()
+	if err != nil {
+		panic(err)
+	}
+	server := http.Server{
+		Addr:      ":8443",
+		Handler:   router,
+		TLSConfig: &tls.Config{Certificates: []tls.Certificate{*cert}, MinVersion: tls.VersionTLS12},
+	}
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+		panic(err)
+	}
+}
+
+func needToRewrite(path string) bool {
+	for _, p := range rewritePathPrefixes {
+		if strings.HasPrefix(path, p) {
+			return true
+		}
+	}
+	return false
+}
+
+func UrlRewrite(r *gin.Engine) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if needToRewrite(c.Request.URL.Path) {
+			c.Request.URL.Path = "/"
+			r.HandleContext(c)
+		}
+		c.Next()
+	}
+}
