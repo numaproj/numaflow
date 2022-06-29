260a1484a19d37c28d7c53abf476e97b376d66d0
Krithika3 <krithika.vkumar@gmail.com>
2022-06-29 14:57:16 -0700

    chore: added yarn build to docker file (#81)
    * chore: Added ui build to Dockerfile
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: fixed some other minor bugs
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: added simple images
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: fixed cluster role
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: Removed redundant numaflow-server
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: fixed review comments
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: Fixed warnings
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    * chore: Fixed some review comments
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>
    
    Co-authored-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>

--- ui/src/components/pipeline/graph/Graph.tsx
+++ ui/src/components/pipeline/graph/Graph.tsx
@@ -131,7 +131,7 @@ export default function Graph(props: GraphProps) {
     setEdgeOpen(false);
   };
 
-  const handlePaneClick = (event: MouseEvent) => {
+  const handlePaneClick = () => {
     setShowSpec(true);
     setEdgeOpen(false);
     setNodeOpen(false);
@@ -165,9 +165,8 @@ export default function Graph(props: GraphProps) {
           onNodeClick={handleNodeClick}
           onPaneClick={handlePaneClick}
           fitView
-          zoomOnScroll={false}
-          panOnDrag={false}
-          elevateEdgesOnSelect={true}
+          zoomOnScroll={true}
+          panOnDrag={true}
         />
       </div>
 
