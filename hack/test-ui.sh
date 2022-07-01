7b3913587af26ac0c6cb44e8376e9d9bb1da0ed8
Krithika3 <krithika.vkumar@gmail.com>
2022-06-30 18:52:54 -0700

    chore: make ui a separate command in makefile (#87)
    * chore: added docker and makefile changes
    
    Signed-off-by: Krithika Vijayakumar <krithika_vijayakumar@intuit.com>

---
+++ hack/test-ui.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source $(dirname $0)/library.sh
+header "Test UI"
+
+ensure_node
+ensure_yarn
+
+test_ui() {
+    yarn --cwd ui lint
+    CI=false yarn --cwd ui test
+}
+
+test_ui
