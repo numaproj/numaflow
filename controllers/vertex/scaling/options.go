228ba3216bdfa1b667407be8310e5561f5fea90e
Derek Wang <whynowy@gmail.com>
2022-07-30 19:39:24 -0700

    feat: autoscaling with back pressure factor (#123)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- controllers/vertex/scaling/options.go
+++ controllers/vertex/scaling/options.go
@@ -1,23 +1,38 @@
 package scaling
 
 type options struct {
-	// Number of workers working on autoscaling
+	// Number of workers working on autoscaling.
 	workers int
 	// Time in milliseconds, each element in the work queue will be picked up in an interval of this period of time.
 	taskInterval int
+	// Threshold of considering there's back pressure, a float value less than 1.
+	backPressureThreshold float64
 }
 
 type Option func(*options)
 
 func defaultOptions() *options {
 	return &options{
-		workers:      20,
-		taskInterval: 30000,
+		workers:               20,
+		taskInterval:          30000,
+		backPressureThreshold: 0.9,
 	}
 }
 
 func WithWorkers(n int) Option {
 	return func(o *options) {
 		o.workers = n
+	}
+}
+
+func WithTaskInterval(n int) Option {
+	return func(o *options) {
+		o.taskInterval = n
+	}
+}
+
+func WithBackPressureThreshold(n float64) Option {
+	return func(o *options) {
+		o.backPressureThreshold = n
 	}
 }
