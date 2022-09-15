71ca00a1b425706d7ef7bbc6f5f19ff8f2718305
Vigith Maurice <vigith@gmail.com>
2022-09-14 18:10:49 -0700

    feat: windowing operations (#157)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>
    Signed-off-by: ashwinidulams <ashttk@gmail.com>
    Co-authored-by: ashwinidulams <ashttk@gmail.com>
    Co-authored-by: ashwinidulams <61725106+ashwinidulams@users.noreply.github.com>

---
+++ pkg/window/keyed/keyed_test.go
@@ -0,0 +1,87 @@
+package keyed
+
+import (
+	"testing"
+	"time"
+
+	"github.com/numaproj/numaflow/pkg/window"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeyedWindow_AddKey(t *testing.T) {
+	iw := &window.IntervalWindow{
+		Start: time.Unix(60, 0),
+		End:   time.Unix(120, 0),
+	}
+	kw := NewKeyedWindow(iw)
+	tests := []struct {
+		name         string
+		given        *KeyedWindow
+		input        string
+		expectedKeys []string
+	}{
+		{
+			name:         "no_keys",
+			given:        &KeyedWindow{},
+			input:        "key1",
+			expectedKeys: []string{"key1"},
+		},
+		{
+			name: "with_some_existing_keys",
+			given: &KeyedWindow{
+				Keys: []string{"key2", "key3"},
+			},
+			input:        "key4",
+			expectedKeys: []string{"key2", "key3", "key4"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kw = NewKeyedWindow(iw)
+			kw.Keys = append(kw.Keys, tt.given.Keys...)
+			kw.AddKey(tt.input)
+			assert.ElementsMatch(t, kw.Keys, tt.expectedKeys)
+		})
+	}
+}
+
+func TestKeyedWindow_Partitions(t *testing.T) {
+	iw := &window.IntervalWindow{
+		Start: time.Unix(60, 0),
+		End:   time.Unix(120, 0),
+	}
+	kw := NewKeyedWindow(iw)
+	tests := []struct {
+		name     string
+		given    *KeyedWindow
+		input    string
+		expected []string
+	}{
+		{
+			name:     "no_keys",
+			given:    &KeyedWindow{},
+			expected: []string{},
+		},
+		{
+			name: "with_some_existing_keys",
+			given: &KeyedWindow{
+				Keys: []string{"key2", "key3"},
+			},
+			expected: []string{
+				"60-120-key2",
+				"60-120-key3",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kw.Keys = tt.given.Keys
+			ret := kw.Partitions()
+			for idx, s := range tt.expected {
+				assert.EqualValues(t, ret[idx], s)
+			}
+		})
+	}
+}
