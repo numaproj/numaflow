65cb762c1b6546a465e2435d72a3b7bcf44a0a65
Keran Yang <yangkr920208@gmail.com>
2022-10-28 11:55:00 -0400

    clean up
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- test/fixtures/util.go
+++ test/fixtures/util.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"os"
 	"os/exec"
 	"regexp"
@@ -20,6 +19,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 )
 
