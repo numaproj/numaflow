fe75e74205e22b46c7ac8ff9b59436d692b14345
Derek Wang <whynowy@gmail.com>
2022-10-31 23:50:42 -0700

    refactor: abstract pod template
    Signed-off-by: Derek Wang <whynowy@gmail.com>

---
+++ pkg/apis/numaflow/v1alpha1/pod_template.go
@@ -0,0 +1,54 @@
+package v1alpha1
+
+import corev1 "k8s.io/api/core/v1"
+
+// AbstractPodTemplate provides a template for pod customization in vertices, daemon deployments and so on.
+type AbstractPodTemplate struct {
+	// Metadata sets the pods's metadata, i.e. annotations and labels
+	// +optional
+	Metadata *Metadata `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// NodeSelector is a selector which must be true for the pod to fit on a node.
+	// Selector which must match a node's labels for the pod to be scheduled on that node.
+	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+	// +optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty" protobuf:"bytes,2,rep,name=nodeSelector"`
+	// If specified, the pod's tolerations.
+	// +optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty" protobuf:"bytes,3,rep,name=tolerations"`
+	// SecurityContext holds pod-level security attributes and common container settings.
+	// Optional: Defaults to empty.  See type description for default values of each field.
+	// +optional
+	SecurityContext *corev1.PodSecurityContext `json:"securityContext,omitempty" protobuf:"bytes,4,opt,name=securityContext"`
+	// ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec.
+	// If specified, these secrets will be passed to individual puller implementations for them to use. For example,
+	// in the case of docker, only DockerConfig type secrets are honored.
+	// More info: https://kubernetes.io/docs/concepts/containers/images#specifying-imagepullsecrets-on-a-pod
+	// +optional
+	// +patchMergeKey=name
+	// +patchStrategy=merge
+	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,5,rep,name=imagePullSecrets"`
+	// If specified, indicates the Redis pod's priority. "system-node-critical"
+	// and "system-cluster-critical" are two special keywords which indicate the
+	// highest priorities with the former being the highest priority. Any other
+	// name must be defined by creating a PriorityClass object with that name.
+	// If not specified, the pod priority will be default or zero if there is no
+	// default.
+	// More info: https://kubernetes.io/docs/concepts/configuration/pod-priority-preemption/
+	// +optional
+	PriorityClassName string `json:"priorityClassName,omitempty" protobuf:"bytes,6,opt,name=priorityClassName"`
+	// The priority value. Various system components use this field to find the
+	// priority of the Redis pod. When Priority Admission Controller is enabled,
+	// it prevents users from setting this field. The admission controller populates
+	// this field from PriorityClassName.
+	// The higher the value, the higher the priority.
+	// More info: https://kubernetes.io/docs/concepts/configuration/pod-priority-preemption/
+	// +optional
+	Priority *int32 `json:"priority,omitempty" protobuf:"bytes,7,opt,name=priority"`
+	// The pod's scheduling constraints
+	// More info: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/
+	// +optional
+	Affinity *corev1.Affinity `json:"affinity,omitempty" protobuf:"bytes,8,opt,name=affinity"`
+	// ServiceAccountName to apply to the StatefulSet
+	// +optional
+	ServiceAccountName string `json:"serviceAccountName,omitempty" protobuf:"bytes,9,opt,name=serviceAccountName"`
+}
--- pkg/apis/numaflow/v1alpha1/redis_buffer_service.go
+++ pkg/apis/numaflow/v1alpha1/redis_buffer_service.go
@@ -54,55 +54,11 @@ type NativeRedis struct {
 	MetricsContainerTemplate *ContainerTemplate `json:"metricsContainerTemplate,omitempty" protobuf:"bytes,5,opt,name=metricsContainerTemplate"`
 	// +optional
 	Persistence *PersistenceStrategy `json:"persistence,omitempty" protobuf:"bytes,6,opt,name=persistence"`
-	// Metadata sets the pods's metadata, i.e. annotations and labels
-	Metadata *Metadata `json:"metadata,omitempty" protobuf:"bytes,7,opt,name=metadata"`
-	// NodeSelector is a selector which must be true for the pod to fit on a node.
-	// Selector which must match a node's labels for the pod to be scheduled on that node.
-	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
 	// +optional
-	NodeSelector map[string]string `json:"nodeSelector,omitempty" protobuf:"bytes,8,rep,name=nodeSelector"`
-	// If specified, the pod's tolerations.
-	// +optional
-	Tolerations []corev1.Toleration `json:"tolerations,omitempty" protobuf:"bytes,9,rep,name=tolerations"`
-	// SecurityContext holds pod-level security attributes and common container settings.
-	// Optional: Defaults to empty.  See type description for default values of each field.
-	// +optional
-	SecurityContext *corev1.PodSecurityContext `json:"securityContext,omitempty" protobuf:"bytes,10,opt,name=securityContext"`
-	// ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec.
-	// If specified, these secrets will be passed to individual puller implementations for them to use. For example,
-	// in the case of docker, only DockerConfig type secrets are honored.
-	// More info: https://kubernetes.io/docs/concepts/containers/images#specifying-imagepullsecrets-on-a-pod
-	// +optional
-	// +patchMergeKey=name
-	// +patchStrategy=merge
-	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,11,rep,name=imagePullSecrets"`
-	// If specified, indicates the Redis pod's priority. "system-node-critical"
-	// and "system-cluster-critical" are two special keywords which indicate the
-	// highest priorities with the former being the highest priority. Any other
-	// name must be defined by creating a PriorityClass object with that name.
-	// If not specified, the pod priority will be default or zero if there is no
-	// default.
-	// More info: https://kubernetes.io/docs/concepts/configuration/pod-priority-preemption/
-	// +optional
-	PriorityClassName string `json:"priorityClassName,omitempty" protobuf:"bytes,12,opt,name=priorityClassName"`
-	// The priority value. Various system components use this field to find the
-	// priority of the Redis pod. When Priority Admission Controller is enabled,
-	// it prevents users from setting this field. The admission controller populates
-	// this field from PriorityClassName.
-	// The higher the value, the higher the priority.
-	// More info: https://kubernetes.io/docs/concepts/configuration/pod-priority-preemption/
-	// +optional
-	Priority *int32 `json:"priority,omitempty" protobuf:"bytes,13,opt,name=priority"`
-	// The pod's scheduling constraints
-	// More info: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/
-	// +optional
-	Affinity *corev1.Affinity `json:"affinity,omitempty" protobuf:"bytes,14,opt,name=affinity"`
-	// ServiceAccountName to apply to the StatefulSet
-	// +optional
-	ServiceAccountName string `json:"serviceAccountName,omitempty" protobuf:"bytes,15,opt,name=serviceAccountName"`
+	AbstractPodTemplate `json:",inline" protobuf:"bytes,7,opt,name=abstractPodTemplate"`
 	// Redis configuration, if not specified, global settings in numaflow-controller-config will be used.
 	// +optional
-	Settings *RedisSettings `json:"settings,omitempty" protobuf:"bytes,16,opt,name=settings"`
+	Settings *RedisSettings `json:"settings,omitempty" protobuf:"bytes,8,opt,name=settings"`
 }
 
 type RedisSettings struct {
