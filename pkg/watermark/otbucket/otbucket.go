b035fab51edbb6433068770812062e5646736045
jyu6 <juanlu_yu@intuit.com>
2022-11-02 21:31:58 -0700

    add comments
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/watermark/otbucket/otbucket.go
+++ pkg/watermark/otbucket/otbucket.go
@@ -11,6 +11,7 @@ type OTValue struct {
 	Watermark int64
 }
 
+// EncodeToBytes encodes a OTValue object into byte array.
 func (v OTValue) EncodeToBytes() ([]byte, error) {
 	buf := bytes.Buffer{}
 	enc := gob.NewEncoder(&buf)
@@ -21,6 +22,7 @@ func (v OTValue) EncodeToBytes() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// DecodeToOTValue decodes the given byte array into a OTValue object.
 func DecodeToOTValue(b []byte) (OTValue, error) {
 	v := OTValue{}
 	dec := gob.NewDecoder(bytes.NewReader(b))
