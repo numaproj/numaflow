b4758185048b5ed8e1c7b46ae2ebf3da16bf419f
Derek Wang <whynowy@gmail.com>
2022-11-03 00:01:37 -0700

    codegen
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/isbsvc/redis_service_test.go
+++ pkg/isbsvc/redis_service_test.go
@@ -1,3 +1,5 @@
+//go:build isb_redis
+
 /*
 Copyright 2022 The Numaproj Authors.
 
@@ -13,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build isb_redis
 
 package isbsvc
 
--- test/sdks-e2e/sdks_test.go
+++ test/sdks-e2e/sdks_test.go
@@ -1,3 +1,5 @@
+//go:build test
+
 /*
 Copyright 2022 The Numaproj Authors.
 
@@ -13,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build test
 
 package sdks_e2e
 
