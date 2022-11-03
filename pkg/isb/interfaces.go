4210bccea71284aeaa183257249f8ee47174f17b
Derek Wang <whynowy@gmail.com>
2022-11-02 23:50:09 -0700

    pkg
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/isb/interfaces.go
+++ pkg/isb/interfaces.go
@@ -1,4 +1,20 @@
 /*
+Copyright 2022 The Numaproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
 Package isb defines and implements the inter-step buffer and the communication. The inter-step communication is reading from the
 previous step (n-1th vertex in the DAG), processing it, conditionally forwarding to zero, one or all the neighboring steps (nth vertices)
 and then acknowledging back to the previous step that we are done with processing.
--- pkg/window/strategy/fixed/fixed.go
+++ pkg/window/strategy/fixed/fixed.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The Numaproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Package fixed implements Fixed windows. Fixed windows (sometimes called tumbling windows) are
 // defined by a static window size, e.g. minutely windows or hourly windows. They are generally aligned, i.e. every
 // window applies across all the data for the corresponding period of time.
