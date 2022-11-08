70bfecb6f498486be83fdab6200f479a33a81adc
Vigith Maurice <vigith@gmail.com>
2022-11-07 18:01:23 -0800

    feat: add blackhole sink
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

--- pkg/apis/numaflow/v1alpha1/sink.go
+++ pkg/apis/numaflow/v1alpha1/sink.go
@@ -21,9 +21,10 @@ import (
 )
 
 type Sink struct {
-	Log    *Log       `json:"log,omitempty" protobuf:"bytes,1,opt,name=log"`
-	Kafka  *KafkaSink `json:"kafka,omitempty" protobuf:"bytes,2,opt,name=kafka"`
-	UDSink *UDSink    `json:"udsink,omitempty" protobuf:"bytes,3,opt,name=udsink"`
+	Log       *Log       `json:"log,omitempty" protobuf:"bytes,1,opt,name=log"`
+	Kafka     *KafkaSink `json:"kafka,omitempty" protobuf:"bytes,2,opt,name=kafka"`
+	Blackhole *Blackhole `json:"blackhole,omitempty" protobuf:"bytes,3,opt,name=blackhole"`
+	UDSink    *UDSink    `json:"udsink,omitempty" protobuf:"bytes,4,opt,name=udsink"`
 }
 
 func (s Sink) getContainers(req getContainerReq) ([]corev1.Container, error) {
