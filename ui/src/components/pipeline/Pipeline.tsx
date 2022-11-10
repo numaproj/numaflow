949701e87992bedced10589732498b998643cb29
DevilAeron <guptavedant2312@gmail.com>
2022-11-10 20:25:39 +0530

    Fixed timestamp in UI to display milliseconds

--- ui/src/components/pipeline/Pipeline.tsx
+++ ui/src/components/pipeline/Pipeline.tsx
@@ -134,8 +134,8 @@ export function Pipeline() {
               vertexWatermark.isWaterMarkEnabled = json["isWatermarkEnabled"];
               vertexWatermark.watermark = json["watermark"];
               vertexWatermark.watermarkLocalTime = new Date(
-                json["watermark"]
-              ).toLocaleString();
+                vertexWatermark.watermark
+              ).toISOString();
               vertexToWatermarkMap.set(vertex.name, vertexWatermark);
             });
         })
