0148258daa7b87fca65f04f70b8d769bb7796468
Keran Yang <yangkr920208@gmail.com>
2022-12-20 10:23:15 -0500

    refactor: a prototype for enhancing E2E test framework (#424)
    This code change demonstrate a prototype to enhance current E2E testing framework. The prototype includes the following two changes and updates an existing test case - TestFiltering.
    
    1. Send data to input http vertex through E2E api instead of http vertex port forwarding.
    2. Replace log verification with checking output data in redis sink.
    
    Next step is to iteratively generalize the testing APIs to meet requirements from all existing and future test cases. e.g. supporting specifying message headers, support per-sink data validations etc.
    
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

---
+++ test/e2e-api/redis.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2022 The Numaproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var redisClient *redis.Client
+
+func init() {
+
+	// When we use this API to validate e2e test result, we always assume a redis UDSink is used
+	// to persist data to a redis instance listening on port 6379.
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+
+	// get-msg-count-contains takes a targetRegex and returns number of keys in redis
+	// which contain a substring matching the targetRegex.
+	http.HandleFunc("/redis/get-msg-count-contains", func(w http.ResponseWriter, r *http.Request) {
+		targetRegex, err := url.QueryUnescape(r.URL.Query().Get("targetRegex"))
+
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Redis Keys API uses scan to retrieve data, which is not best practice in terms of performance.
+		// TODO - Look into replacing it with a more efficient API or data structure.
+		keyList, err := redisClient.Keys(context.Background(), fmt.Sprintf("*%s*", targetRegex)).Result()
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(200)
+		_, _ = w.Write([]byte(fmt.Sprint(len(keyList))))
+	})
+}
