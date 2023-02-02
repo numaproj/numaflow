cf2fce47de935ded567cd90133a00f0050b32fe2
jyu6 <juanlu_yu@intuit.com>
2023-02-01 21:00:10 -0800

    add a comment
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/watermark/fetch/processor_manager.go
+++ pkg/watermark/fetch/processor_manager.go
@@ -252,6 +252,7 @@ func (v *ProcessorManager) startTimeLineWatcher() {
 				if otValue.Idle {
 					var processors = v.GetAllProcessors()
 					for processorName, processor := range processors {
+						// skip the processor itself, only use other processors as reference
 						if processorName != value.Key() {
 							// any other Vn-1 processor's non-empty tail offsetWatermark can replace the idle watermark
 							// if all tail offsetWatermarks are empty, then it means there's no data flowing into
