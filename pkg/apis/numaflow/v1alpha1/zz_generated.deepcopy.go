2d53e92f631b3c1a7191c4c2baa328ed76d39de3
Derek Wang <whynowy@gmail.com>
2023-02-08 14:30:47 -0800

    chore: SecurityContext for user defined containers (#532)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/apis/numaflow/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/numaflow/v1alpha1/zz_generated.deepcopy.go
@@ -294,6 +294,11 @@ func (in *Container) DeepCopyInto(out *Container) {
 		}
 	}
 	in.Resources.DeepCopyInto(&out.Resources)
+	if in.SecurityContext != nil {
+		in, out := &in.SecurityContext, &out.SecurityContext
+		*out = new(v1.SecurityContext)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
