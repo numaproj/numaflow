6604d59aeee1eee873113b0eccf1231db0de318b
jyu6 <juanlu_yu@intuit.com>
2023-02-08 17:46:05 -0800

    add err
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/sinks/sink.go
+++ pkg/sinks/sink.go
@@ -113,8 +113,11 @@ func (u *SinkProcessor) Start(ctx context.Context) error {
 	}()
 	var metricsOpts []metrics.Option
 	if udSink := u.VertexInstance.Vertex.Spec.Sink.UDSink; udSink != nil {
-		serverHandler := sinker.(*udsink.UserDefinedSink)
-		metricsOpts = metrics.NewMetricsOptions(ctx, u.VertexInstance.Vertex, serverHandler, reader, nil)
+		if serverHandler, ok := sinker.(*udsink.UserDefinedSink); ok {
+			metricsOpts = metrics.NewMetricsOptions(ctx, u.VertexInstance.Vertex, serverHandler, reader, nil)
+		} else {
+			return fmt.Errorf("unable to get the metrics options for the udsink")
+		}
 	} else {
 		metricsOpts = metrics.NewMetricsOptions(ctx, u.VertexInstance.Vertex, nil, reader, nil)
 	}
