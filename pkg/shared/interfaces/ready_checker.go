994138f6fa528f4e00dc494bd2a83de6d40257fe
Juanlu Yu <19543684+chromevoid@users.noreply.github.com>
2023-02-09 10:44:26 -0800

    Update ready_checker.go

--- pkg/shared/interfaces/ready_checker.go
+++ pkg/shared/interfaces/ready_checker.go
@@ -2,6 +2,8 @@ package interfaces
 
 import "context"
 
+// ReadyChecker is the interface to check if the server is ready
 type ReadyChecker interface {
+	// WaitUntilReady waits for the server to get ready
 	WaitUntilReady(ctx context.Context) error
 }
