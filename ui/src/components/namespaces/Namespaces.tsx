34ff23cdb408792e0bb4f1987893a21d66729f02
veds-g <guptavedant2312@gmail.com>
2023-02-21 12:51:00 +0530

    adding namespace search for namespace scope installation of UI server
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- ui/src/components/namespaces/Namespaces.tsx
+++ ui/src/components/namespaces/Namespaces.tsx
@@ -1,22 +1,23 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { useFetch } from "../../utils/fetchWrappers/fetch";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { NamespaceRowContent } from "./NamespaceRowContent";
-import IconButton from "@mui/material/IconButton";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import Collapse from "@mui/material/Collapse";
 import "./Namespaces.css";
-
+import {Button, TableBody, Table, TableCell, TableContainer, TableHead, TableRow, Paper, TextField} from "@mui/material";
+import ClearIcon from '@mui/icons-material/Clear'
+import SearchIcon from '@mui/icons-material/Search';
 
 export function Namespaces() {
-  const { data } = useFetch("/api/v1/namespaces");
+    const [value, setValue] = useState("");
+    const [namespace, setNamespace] = useState("");
+    const handle = (namespaceVal) => {
+        localStorage.setItem("namespace", namespaceVal);
+    };
+    useEffect(() => {
+        const prevVal = localStorage.getItem("namespace");
+        if (prevVal) {
+            setValue(prevVal);
+            setNamespace(prevVal);
+        }
+    }, []);
 
   return (
     <div className="Namespaces">
@@ -24,71 +25,52 @@ export function Namespaces() {
         <Table aria-label="collapsible table">
           <TableHead>
             <TableRow>
-              <TableCell />
               <TableCell
-                style={{
-                  fontWeight: "bold",
-                  fontSize: "1rem",
-                }}
               >
-                Namespace
+                  <div style={{display: "flex", flexDirection: "row"}}>
+                  <TextField
+                      InputLabelProps={{ shrink: true }}
+                      label="Namespace"
+                      placeholder="enter a namespace"
+                      variant="standard"
+                      style={{width: "200px"}}
+                      value={value}
+                      onChange={e => {setValue(e.target.value)}}
+                  />
+                  <Button
+                      data-testid="namespace-search"
+                      onClick={() => {
+                          handle(value);
+                          setNamespace(value);
+                      }}
+                      style={{marginTop: "15px", height: "30px"}}
+                  >
+                      <SearchIcon/>
+                  </Button>
+                  <Button
+                      data-testid="namespace-clear"
+                      onClick={() => {
+                          handle("");
+                          setNamespace("");
+                          setValue("");
+                      }}
+                      style={{marginTop: "15px", height: "30px"}}
+                  >
+                      <ClearIcon/>
+                  </Button>
+                  </div>
               </TableCell>
             </TableRow>
           </TableHead>
-            {data &&
-              data.map((namespace: string) => {
-                return <NamespaceRow key={namespace} namespaceId={namespace} />;
-              })}
+          <TableBody>
+            <TableRow>
+                <TableCell>
+                    <NamespaceRowContent namespaceId={namespace} />
+                </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
   );
 }
-
-interface NamespaceRowProps {
-    namespaceId: string;
-}
-
-export function NamespaceRow(props: NamespaceRowProps) {
-    const { namespaceId } = props;
-    const [open, setOpen] = useState(false);
-
-    return (
-        <TableBody data-testid={`namespace-row-body-${namespaceId}`}>
-            <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-                <TableCell>
-                    <IconButton
-                        data-testid={`namespace-row-${namespaceId}`}
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell
-                    sx={{
-                        fontWeight: 500,
-                        fontSize: "1rem",
-                    }}
-                    component="th"
-                    scope="row"
-                >
-                    {namespaceId}
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell
-                    data-testid="table-cell"
-                    style={{ paddingBottom: 0, paddingTop: 0 }}
-                    colSpan={6}
-                >
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <NamespaceRowContent namespaceId={namespaceId} />
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </TableBody>
-    );
-}
-
