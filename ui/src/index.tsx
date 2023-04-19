7f6e50f379501ef99beedfa67551f2c4c856a10e
Dillen Padhiar <dillen_padhiar@intuit.com>
2023-04-19 14:01:58 -0700

    fix: remove unused useEffect import
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
