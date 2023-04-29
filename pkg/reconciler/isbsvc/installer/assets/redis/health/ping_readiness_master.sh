494b4a1144a4c59acc3766dd381b7aec04410b9d
Julie Vogelmani <julie_vogelman@intuit.com>
2023-04-29 15:55:10 -0700

    health script updates
    Signed-off-by: Julie Vogelmani <julie_vogelman@intuit.com>

--- pkg/reconciler/isbsvc/installer/assets/redis/health/ping_readiness_master.sh
+++ pkg/reconciler/isbsvc/installer/assets/redis/health/ping_readiness_master.sh
@@ -1,22 +1,22 @@
 
-  #!/bin/bash
-  [[ -f $REDIS_MASTER_PASSWORD_FILE ]] && export REDIS_MASTER_PASSWORD="$(<
-  "${REDIS_MASTER_PASSWORD_FILE}")"
-  [[ -n "$REDIS_MASTER_PASSWORD" ]] && export
-  REDISCLI_AUTH="$REDIS_MASTER_PASSWORD"
-  response=$(
-    timeout -s 15 $1 \
-    redis-cli \
-      -h $REDIS_MASTER_HOST \
-      -p $REDIS_MASTER_PORT_NUMBER \
-      ping
-  )
-  if [ "$?" -eq "124" ]; then
-    echo "Timed out"
-    exit 1
-  fi
+#!/bin/bash
+[[ -f $REDIS_MASTER_PASSWORD_FILE ]] && export REDIS_MASTER_PASSWORD="$(<
+"${REDIS_MASTER_PASSWORD_FILE}")"
+[[ -n "$REDIS_MASTER_PASSWORD" ]] && export
+REDISCLI_AUTH="$REDIS_MASTER_PASSWORD"
+response=$(
+  timeout -s 15 $1 \
+  redis-cli \
+    -h $REDIS_MASTER_HOST \
+    -p $REDIS_MASTER_PORT_NUMBER \
+    ping
+)
+if [ "$?" -eq "124" ]; then
+  echo "Timed out"
+  exit 1
+fi
 
-  if [ "$response" != "PONG" ]; then
-    echo "$response"
-    exit 1
-  fi
+if [ "$response" != "PONG" ]; then
+  echo "$response"
+  exit 1
+fi
