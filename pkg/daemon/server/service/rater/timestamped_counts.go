97db1984ac5f14cd53e4d09b4ceb827f45b23f2e
Keran Yang <yangkr920208@gmail.com>
2023-06-16 12:23:13 -0400

    refactor: remove redundant delta calculations for rater (#795)
    When we calculate processing rate, we calculate deltas between all pairs of timestamped counts within lookback seconds. If two consecutive timestamped counts all finished collecting metrics, then the delta calculation should only execute once. This PR introduce the concept of `window`. When a timestamp counts finish collecting counts, we mark the it as window-closed and calculate the delta and save the delta. Such that future GetRate calls won't need to re-calculate the delta and instead directly use the delta attribute of that closed window. This brings down the time complexity of calculating vertex level rate from O(lookback seconds * no. of pods) to O(lookback seconds)
    
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- pkg/daemon/server/service/rater/timestamped_counts.go
+++ pkg/daemon/server/service/rater/timestamped_counts.go
@@ -31,17 +31,24 @@ type TimestampedCounts struct {
 	timestamp int64
 	// podName to count mapping
 	podCounts map[string]float64
-	lock      *sync.RWMutex
+	// isWindowClosed indicates whether we have finished collecting pod counts for this timestamp
+	isWindowClosed bool
+	// delta is the total count change from the previous window, it's valid only when isWindowClosed is true
+	delta float64
+	lock  *sync.RWMutex
 }
 
 func NewTimestampedCounts(t int64) *TimestampedCounts {
 	return &TimestampedCounts{
-		timestamp: t,
-		podCounts: make(map[string]float64),
-		lock:      new(sync.RWMutex),
+		timestamp:      t,
+		podCounts:      make(map[string]float64),
+		isWindowClosed: false,
+		delta:          0,
+		lock:           new(sync.RWMutex),
 	}
 }
 
+// Update updates the count for a pod if the current window is not closed
 func (tc *TimestampedCounts) Update(podName string, count float64) {
 	tc.lock.Lock()
 	defer tc.lock.Unlock()
@@ -54,6 +61,10 @@ func (tc *TimestampedCounts) Update(podName string, count float64) {
 		// when calculating the rate, as we sum up deltas among timestamps, we will get 123457 total delta instead of the real delta 1.
 		// one occurrence of such case can lead to extremely high rate and mess up the autoscaling.
 		// hence we'd rather keep the count as it is to avoid wrong rate calculation.
+		return
+	}
+	if tc.isWindowClosed {
+		// we skip updating if the window is already closed.
 		return
 	}
 	tc.podCounts[podName] = count
@@ -69,6 +80,44 @@ func (tc *TimestampedCounts) Snapshot() map[string]float64 {
 		counts[k] = v
 	}
 	return counts
+}
+
+// IsWindowClosed returns whether the window is closed
+func (tc *TimestampedCounts) IsWindowClosed() bool {
+	tc.lock.RLock()
+	defer tc.lock.RUnlock()
+	return tc.isWindowClosed
+}
+
+// CloseWindow closes the window and calculates the delta by comparing the current pod counts with the previous window
+func (tc *TimestampedCounts) CloseWindow(prev *TimestampedCounts) {
+	// prepare pod counts for both current and previous window for delta calculation
+	var prevPodCounts map[string]float64
+	if prev == nil {
+		prevPodCounts = make(map[string]float64)
+	} else {
+		prevPodCounts = prev.Snapshot()
+	}
+	currPodCounts := tc.Snapshot()
+
+	// calculate the delta by comparing the current pod counts with the previous window
+	delta := 0.0
+	for key, currCount := range currPodCounts {
+		prevCount := prevPodCounts[key] // if key doesn't exist in prevPodCounts, prevCount is 0
+		if currCount < prevCount {
+			// this can happen when a pod is restarted during the window
+			// we count the new count as the delta
+			delta += currCount
+		} else {
+			delta += currCount - prevCount
+		}
+	}
+
+	// finalize the window by setting isWindowClosed to true and delta to the calculated value
+	tc.lock.Lock()
+	defer tc.lock.Unlock()
+	tc.isWindowClosed = true
+	tc.delta = delta
 }
 
 // ToString returns a string representation of the TimestampedCounts
