7c514ae53fb3f8bdfc5fcf0b568c3b611942181b
Derek Wang <whynowy@gmail.com>
2023-07-17 15:22:24 -0700

    x
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/apis/numaflow/v1alpha1/vertex_types.go
+++ pkg/apis/numaflow/v1alpha1/vertex_types.go
@@ -287,7 +287,6 @@ func (v Vertex) GetPodSpec(req GetVertexPodSpecReq) (*corev1.PodSpec, error) {
 		}
 		containers = append(containers, sideInputsWatcher)
 		for i := 1; i < len(containers); i++ {
-			containers[i].VolumeMounts = append(containers[i].VolumeMounts, corev1.VolumeMount{Name: sideInputsVolName, MountPath: PathSideInputsMount})
 			if containers[i].Name == CtrSideInputsWatcher {
 				containers[i].VolumeMounts = append(containers[i].VolumeMounts, corev1.VolumeMount{Name: sideInputsVolName, MountPath: PathSideInputsMount})
 			} else {
