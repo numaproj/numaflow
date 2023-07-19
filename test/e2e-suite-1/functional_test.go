28b856ef06472ba8eac3993899c139c6b9c0d8d8
Julie Vogelman <julievogelman0@gmail.com>
2023-07-19 14:09:13 -0700

    move test to other suite
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- test/e2e-suite-1/functional_test.go
+++ test/e2e-suite-1/functional_test.go
@@ -309,23 +309,6 @@ func (s *FunctionalSuite) TestConditionalForwarding() {
 	w.Expect().SinkNotContains("number-sink", "not an integer")
 }
 
-func (s *FunctionalSuite) TestJoinSinkVertex() {
-	w := s.Given().Pipeline("@testdata/join-on-sink.yaml").
-		When().
-		CreatePipelineAndWait()
-	defer w.DeletePipelineAndWait()
-	pipelineName := "join-on-sink"
-
-	// wait for all the pods to come up
-	w.Expect().VertexPodsRunning()
-
-	w.SendMessageTo(pipelineName, "in", NewHttpPostRequest().WithBody([]byte("888888"))).
-		SendMessageTo(pipelineName, "in", NewHttpPostRequest().WithBody([]byte("888889")))
-
-	w.Expect().SinkContains("out", "888888")
-	w.Expect().SinkContains("out", "888889")
-}
-
 func (s *FunctionalSuite) TestWatermarkEnabled() {
 	w := s.Given().Pipeline("@testdata/watermark.yaml").
 		When().
