3c21a6a371924eb2653c18c5effffa6f4b9f11b3
veds-g <guptavedant2312@gmail.com>
2023-08-01 14:31:41 +0530

    added watermark delay
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- ui/src/components/pages/Pipeline/partials/Graph/partials/CustomEdge/index.tsx
+++ ui/src/components/pages/Pipeline/partials/Graph/partials/CustomEdge/index.tsx
@@ -50,6 +50,51 @@ const CustomEdge: FC<EdgeProps> = ({
       labelY = targetY + 2;
     }
   }
+  const getMinWM = () => {
+    if (data?.edgeWatermark?.watermarks) {
+      return Math.min.apply(null, data?.edgeWatermark?.watermarks);
+    } else {
+      return -1;
+    }
+  };
+
+  const getDelay = () => {
+    const str = "Delay - ";
+    const fetchWMTime = data?.edgeWatermark?.WMFetchTime;
+    const minWM = getMinWM();
+    let difference =
+      (fetchWMTime ? fetchWMTime : Date.now()) - (minWM == -1 ? 0 : minWM);
+
+    const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
+    difference -= years * (1000 * 60 * 60 * 24 * 365);
+    const months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30));
+    difference -= months * (1000 * 60 * 60 * 24 * 30);
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    difference -= days * (1000 * 60 * 60 * 24);
+    const hours = Math.floor(difference / (1000 * 60 * 60));
+    difference -= hours * (1000 * 60 * 60);
+    const minutes = Math.floor(difference / (1000 * 60));
+    difference -= minutes * (1000 * 60);
+    const seconds = Math.floor(difference / 1000);
+    difference -= seconds * 1000;
+    const milliseconds = difference;
+
+    if (years > 0) {
+      return str + `${years}yr ${months}mo`;
+    } else if (months > 0) {
+      return str + `${months}mo ${days}d`;
+    } else if (days > 0) {
+      return str + `${days}d ${hours}hr`;
+    } else if (hours > 0) {
+      return str + `${hours}hr ${minutes}min`;
+    } else if (minutes > 0) {
+      return str + `${minutes}min ${seconds}sec`;
+    } else if (seconds > 0) {
+      return str + `${seconds}sec ${milliseconds}ms`;
+    } else {
+      return str + `${milliseconds}ms`;
+    }
+  };
 
   return (
     <>
@@ -74,18 +119,15 @@ const CustomEdge: FC<EdgeProps> = ({
               title={
                 <div className={"edge-tooltip"}>
                   <div>Watermark</div>
-                  <div>
-                    {new Date(
-                      Math.min.apply(null, data?.edgeWatermark?.watermarks)
-                    ).toISOString()}
-                  </div>
+                  <div>{new Date(getMinWM()).toISOString()}</div>
+                  <div>{getDelay()}</div>
                 </div>
               }
               arrow
               placement={"top"}
             >
               <div className={"edge-label"} style={wmStyle}>
-                {Math.min.apply(null, data?.edgeWatermark?.watermarks)}
+                {getMinWM()}
               </div>
             </Tooltip>
           )}
