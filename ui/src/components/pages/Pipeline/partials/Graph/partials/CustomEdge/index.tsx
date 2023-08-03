9539a3a11f8085073bda6052e0ae1a9b5ac725b9
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2023-08-03 10:21:03 +0530

    feat: watermark delay in tooltip (#910)
    Signed-off-by: Jason Zesheng Chen <64990318+jasonzeshengchen@users.noreply.github.com>

--- ui/src/components/pages/Pipeline/partials/Graph/partials/CustomEdge/index.tsx
+++ ui/src/components/pages/Pipeline/partials/Graph/partials/CustomEdge/index.tsx
@@ -1,6 +1,7 @@
 import { FC, memo } from "react";
 import { Tooltip } from "@mui/material";
 import { EdgeProps, EdgeLabelRenderer, getSmoothStepPath } from "reactflow";
+import { duration } from "moment";
 
 import "reactflow/dist/style.css";
 import "./style.css";
@@ -50,6 +51,46 @@ const CustomEdge: FC<EdgeProps> = ({
       labelY = targetY + 2;
     }
   }
+  const getMinWM = () => {
+    if (data?.edgeWatermark?.watermarks) {
+      return Math.min.apply(null, data?.edgeWatermark?.watermarks);
+    } else {
+      return -1;
+    }
+  };
+
+  const getDelay = () => {
+    const str = " behind now";
+    const fetchWMTime = data?.edgeWatermark?.WMFetchTime;
+    const minWM = getMinWM();
+    const startTime = minWM === -1 ? 0 : minWM;
+    const endTime = fetchWMTime || Date.now();
+    const diff = duration(endTime - startTime);
+
+    const years = diff.years();
+    const months = diff.months();
+    const days = diff.days();
+    const hours = diff.hours();
+    const minutes = diff.minutes();
+    const seconds = diff.seconds();
+    const milliseconds = diff.milliseconds();
+
+    if (years > 0) {
+      return `${years}yr ${months}mo` + str;
+    } else if (months > 0) {
+      return `${months}mo ${days}d` + str;
+    } else if (days > 0) {
+      return `${days}d ${hours}hr` + str;
+    } else if (hours > 0) {
+      return `${hours}hr ${minutes}min` + str;
+    } else if (minutes > 0) {
+      return `${minutes}min ${seconds}sec` + str;
+    } else if (seconds > 0) {
+      return `${seconds}sec ${milliseconds}ms` + str;
+    } else {
+      return `${milliseconds}ms` + str;
+    }
+  };
 
   return (
     <>
@@ -74,18 +115,15 @@ const CustomEdge: FC<EdgeProps> = ({
               title={
                 <div className={"edge-tooltip"}>
                   <div>Watermark</div>
-                  <div>
-                    {new Date(
-                      Math.min.apply(null, data?.edgeWatermark?.watermarks)
-                    ).toISOString()}
-                  </div>
+                  <div>{new Date(getMinWM()).toISOString()}</div>
+                  <div>{getDelay()}</div>
                 </div>
               }
               arrow
               placement={"top"}
             >
               <div className={"edge-label"} style={wmStyle}>
-                {Math.min.apply(null, data?.edgeWatermark?.watermarks)}
+                {getMinWM()}
               </div>
             </Tooltip>
           )}
