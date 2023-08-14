2c9267c0479a1894f50c6c027be6029dfd9963fb
veds-g <guptavedant2312@gmail.com>
2023-08-14 21:21:12 +0530

    removed side-nav-bar
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,9 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { Breadcrumbs } from "./components/common/Breadcrumbs";
 import { Namespaces } from "./components/pages/Namespace";
 import { Pipeline } from "./components/pages/Pipeline";
@@ -17,8 +11,6 @@ import logo from "./images/icon-on-blue-bg.png";
 import "./App.css";
 import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-const drawerWidth = 240;
 
 function App() {
   return (
@@ -33,7 +25,7 @@ function App() {
         >
           <Box
             sx={{
-              height: "64px",
+              height: "4rem",
             }}
           >
             <AppBar
@@ -48,7 +40,7 @@ function App() {
                   sx={{
                     fontSize: "1.25rem",
                     fontWeight: 500,
-                    marginLeft: "20px",
+                    marginLeft: "1.25rem",
                   }}
                   variant="h6"
                   noWrap
@@ -62,72 +54,28 @@ function App() {
           <Box
             sx={{
               display: "flex",
-              flexDirection: "row",
-              width: "100% ",
+              flexDirection: "column",
+              padding: "1.5rem",
+              width: "100%",
+              overflow: "auto",
             }}
           >
-            <div className="App-side-nav">
-              <Drawer
-                variant="permanent"
-                sx={{
-                  width: drawerWidth,
-                  flexShrink: 0,
-                  [`& .MuiDrawer-paper`]: {
-                    width: drawerWidth,
-                    boxSizing: "border-box",
-                    fontFamily: "IBM Plex Sans",
-                    fontSize: "1rem",
-                    fontWeight: 400,
-                  },
-                }}
-              >
-                <Toolbar />
-
-                <Box
-                  sx={{
-                    overflow: "auto",
-                    backgroundColor: "#f4f5f8",
-                  }}
-                >
-                  <List>
-                    <Link to="/" className="App-side-nav-link">
-                      <ListItem button key="namespaces">
-                        <ListItemIcon>
-                          <InboxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={"Namespaces"} />
-                      </ListItem>
-                    </Link>
-                  </List>
-                </Box>
-              </Drawer>
-            </div>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                padding: "25px",
-                width: "calc(100% - 240px)",
-                overflow: "auto",
-              }}
-            >
-              <Breadcrumbs />
-              <Routes>
-                <Route path="/" element={<Namespaces />} />
-                <Route
-                  path="/namespaces/:namespaceId/pipelines/:pipelineId"
-                  element={<Pipeline />}
-                />
-                <Route
-                  path="*"
-                  element={
-                    <main style={{ padding: "1rem" }}>
-                      <p>There's nothing here!</p>
-                    </main>
-                  }
-                />
-              </Routes>
-            </Box>
+            <Breadcrumbs />
+            <Routes>
+              <Route path="/" element={<Namespaces />} />
+              <Route
+                path="/namespaces/:namespaceId/pipelines/:pipelineId"
+                element={<Pipeline />}
+              />
+              <Route
+                path="*"
+                element={
+                  <main style={{ padding: "1rem" }}>
+                    <p>There's nothing here!</p>
+                  </main>
+                }
+              />
+            </Routes>
           </Box>
         </Box>
       </ScopedCssBaseline>
