8e1aa9c14e56827295deb355d32708254bdb1760
jyu6 <juanlu_yu@intuit.com>
2023-09-12 10:37:54 -0700

    udpate isbsvc response
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- server/apis/v1_1/response_isb_services.go
+++ server/apis/v1_1/response_isb_services.go
@@ -1,1 +1,10 @@
 package v1_1
+
+import "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
+
+// ISBServices is a list of ISBService
+type ISBServices []struct {
+	Name          string                          `json:"name"`
+	HealthyStatus string                          `json:"healthyStatus"`
+	ISBService    v1alpha1.InterStepBufferService `json:"isbService"`
+}
