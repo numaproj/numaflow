8f3625c655f7be06301c875e1084b52097a02b3e
Julie Vogelman <julievogelman0@gmail.com>
2023-09-29 12:03:06 -0700

    remove lingering references to redis streams
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- pkg/reconciler/pipeline/validate.go
+++ pkg/reconciler/pipeline/validate.go
@@ -104,7 +104,7 @@ func ValidatePipeline(pl *dfv1.Pipeline) error {
 				return fmt.Errorf("invalid user-defined source vertex %q, a customized image is required", k)
 			}
 			if s.Source.HTTP != nil || s.Source.Kafka != nil || s.Source.Nats != nil || s.Source.Generator != nil {
-				return fmt.Errorf("invalid user-defined source vertex %q, only one of 'http', 'kafka', 'nats', 'redisStreams', 'generator' and 'udSource' can be specified", k)
+				return fmt.Errorf("invalid user-defined source vertex %q, only one of 'http', 'kafka', 'nats', 'generator' and 'udSource' can be specified", k)
 			}
 		}
 	}
