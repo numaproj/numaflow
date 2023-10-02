f8592973ccb7216a42ac2a58e4d76fd4cb5fd40b
Keran Yang <yangkr920208@gmail.com>
2023-10-02 11:51:30 -0400

    fix: treat ALL user-defined source vertices as scalable (#1132)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>
    Signed-off-by: jmillage <jmillage@bcubed-corp.com>

--- pkg/apis/numaflow/v1alpha1/vertex_types.go
+++ pkg/apis/numaflow/v1alpha1/vertex_types.go
@@ -114,7 +114,7 @@ func (v Vertex) Scalable() bool {
 	}
 	if v.IsASource() {
 		src := v.Spec.Source
-		if src.Kafka != nil {
+		if src.Kafka != nil || src.UDSource != nil {
 			return true
 		}
 	}
