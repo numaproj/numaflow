47ae546b7ae22084ba898f1097201faba8a25296
bbehnke <bradleybehnke@yahoo.com>
2023-10-17 15:14:19 -0700

    fix: route fix
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    fix: redirect work
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    fix: route fix (#1224)
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    feat: user context added
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    fix: reverted some changes
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>

---
+++ ui/src/components/common/AccountMenu/index.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useCallback, useMemo, useContext } from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import IconButton from "@mui/material/IconButton";
+import Logout from "@mui/icons-material/Logout";
+import { AppContextProps } from "../../../types/declarations/app";
+import { AppContext } from "../../../App";
+
+export default function AccountMenu() {
+  const { userInfo } = useContext<AppContextProps>(AppContext);
+  const [anchorEl, setAnchorEl] = useState<undefined | HTMLElement>();
+
+  const handleClick = useCallback((event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setAnchorEl(undefined);
+  }, []);
+
+  const handleLogout = useCallback(() => {
+    handleClose();
+    // TODO
+  }, []);
+
+  const open = useMemo(() => !!anchorEl, [anchorEl]);
+
+  if (!userInfo) {
+    // Non-auth, hide account menu
+    return undefined;
+  }
+
+  return (
+    <React.Fragment>
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ ml: 2 }}
+          aria-controls={open ? "account-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <Avatar sx={{ width: 40, height: 40, backgroundColor: "#00D0E0" }}>
+            {userInfo.name.charAt(0).toUpperCase()}
+          </Avatar>
+        </IconButton>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleLogout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+}
