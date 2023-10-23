ee6747b8434dfc453247a2e0615373f09b4fa4cb
veds-g <guptavedant2312@gmail.com>
2023-10-23 18:52:24 +0530

    enhanced generator vertex
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- ui/src/components/common/SlidingSidebar/partials/GeneratorDetails/index.tsx
+++ ui/src/components/common/SlidingSidebar/partials/GeneratorDetails/index.tsx
@@ -3,7 +3,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { GeneratorUpdate } from "./partials/GeneratorUpdate";
-import generatorIcon from "../../../../../images/generator.png";
+import generatorIcon from "../../../../../images/generator.svg";
 
 import "./style.css";
 
