9a8589be4c2e91b79127c9aac56bd9f3629380ac
jyu6 <juanlu_yu@intuit.com>
2023-10-23 23:28:49 -0700

    Revert " remove dex manifest"
    This reverts commit 004c3550161d2889165b82e38035910b6e9f6136.
    
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- server/apis/v1/dexauth.go
+++ server/apis/v1/dexauth.go
@@ -110,11 +110,11 @@ func (d *DexObject) handleCallback(c *gin.Context) {
 		return
 	}
 	// TODO: currently looks like it only works when only one user does the login
-	// if state := r.FormValue("state"); state != d.stateNonce {
-	// 	errMsg := fmt.Sprintf("Expected state %q got %q", d.stateNonce, state)
-	// 	c.JSON(http.StatusOK, NewNumaflowAPIResponse(&errMsg, nil))
-	// 	return
-	// }
+	if state := r.FormValue("state"); state != d.stateNonce {
+		errMsg := fmt.Sprintf("Expected state %q got %q", d.stateNonce, state)
+		c.JSON(http.StatusOK, NewNumaflowAPIResponse(&errMsg, nil))
+		return
+	}
 
 	token, err = oauth2Config.Exchange(ctx, code)
 	if err != nil {
