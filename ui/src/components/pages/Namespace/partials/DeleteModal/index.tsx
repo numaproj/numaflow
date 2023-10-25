7940c7764a74fbe5fe0fda64e89bdbab6d7899ea
bbehnke <bradleybehnke@yahoo.com>
2023-10-25 10:21:03 -0700

    fix: try again for delete and updated 403 messages
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    fix: no access error msg updates
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>
    
    fix: vertex side bar size
    
    Signed-off-by: bbehnke <bradleybehnke@yahoo.com>

--- ui/src/components/pages/Namespace/partials/DeleteModal/index.tsx
+++ ui/src/components/pages/Namespace/partials/DeleteModal/index.tsx
@@ -102,7 +102,7 @@ export function DeleteModal({
               variant="contained"
               color="secondary"
             >
-              Delete
+              Try again
             </Button>
             <Button onClick={onCancel} variant="outlined" color="primary">
               Cancel
