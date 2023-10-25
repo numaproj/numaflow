0ed39a80ebe6700034e62b750a5d66887f12fb08
mshakira <shakira.git@gmail.com>
2023-10-25 15:54:14 -0700

    fix(SERVER): update gradient test (#1271)
    Signed-off-by: Shakira M <shakira_m@intuit.com>
    Co-authored-by: Shakira M <shakira_m@intuit.com>
    Co-authored-by: Bradley Behnke <bradleybehnke@yahoo.com>

--- ui/src/utils/gradient.test.ts
+++ ui/src/utils/gradient.test.ts
@@ -1,26 +1,34 @@
-import { fill, getColorCode, rgba2rgb } from "./gradients"
+import { fill, getColorCode, imageForStatus, rgba2rgb } from "./gradients";
 
+const cpuColors = {
+  infinite: [100, 100000],
+  red: [76, 1000],
+  orange: [51, 75],
+  yellow: [31, 50],
+  green: [0, 30],
+};
 describe("gradients", () => {
+  it("rgba2rgb", () => {
+    expect(rgba2rgb([255, 255, 255], [127, 208, 0], 1)).toEqual("#7fd000");
+  });
 
-    const cpuColors = {
-        infinite: [100, 100000],
-        red: [76, 1000],
-        orange: [51, 75],
-        yellow: [31, 50],
-        green: [0, 30],
-    };
+  it("getColorCode", () => {
+    expect(getColorCode(cpuColors, 60)).toEqual("orange");
+    expect(getColorCode(cpuColors, 33)).toEqual("yellow");
+    expect(getColorCode(cpuColors, 70)).toEqual("orange");
+    expect(getColorCode(cpuColors, -1)).toEqual("grey");
+    expect(getColorCode(cpuColors, 101)).toEqual("infinite");
+    expect(getColorCode(cpuColors, 0)).toEqual("green");
+  });
 
-    it("rgba2rgb", () => {
-        expect(rgba2rgb([255, 255, 255], [127, 208, 0], 1)).toEqual("#7fd000")
-    })
+  it("fill", () => {
+    expect(fill(cpuColors, 8, 0.5, 10)).toEqual("rgb(247, 87, 108)");
+  });
+});
 
-    it("getColorCode", () => {
-        expect(getColorCode( cpuColors, 60)).toEqual("orange")
-        expect(getColorCode(cpuColors, 33)).toEqual("yellow")
-        expect(getColorCode(cpuColors, 70)).toEqual("orange")
-    })
-
-    it("fill", () => {
-        expect(fill(cpuColors, 8, 0.5, 10)).toEqual("rgb(247, 87, 108)")
-    })
-})
+// write test for imageForStatus
+describe("imageForStatus", () => {
+  it("imageForStatus", () => {
+    expect(imageForStatus(cpuColors, 60, 100)).toEqual("exclamation.svg");
+  });
+});
