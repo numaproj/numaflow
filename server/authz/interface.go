b35f161b023bb4517ebca9bc0cf3cb568de46d0e
Sidhant Kohli <sidhant.kohli@gmail.com>
2023-11-01 11:09:17 -0700

    refactor
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- server/authz/interface.go
+++ server/authz/interface.go
@@ -8,8 +8,6 @@ import (
 
 type Authorizer interface {
 	// Authorize checks if a user is authorized to access the resource.
-	// c is the gin context.
-	// g is the list of groups the user belongs to.
 	// Authorize trusts that the user is already authenticated and directly uses the groups to authorize the user.
 	// please don't use gin to get the user information again.
 	// Authorize returns true if the user is authorized, otherwise false.
