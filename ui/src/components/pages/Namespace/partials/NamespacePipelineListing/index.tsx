ffb453a8d555e65de093bf9c664b006789ce570e
Shakira M <shakira_m@intuit.com>
2023-11-01 16:42:29 -0700

    fix(SERVER): fix status images and add unknown status in filtering
    Signed-off-by: Shakira M <shakira_m@intuit.com>

--- ui/src/components/pages/Namespace/partials/NamespacePipelineListing/index.tsx
+++ ui/src/components/pages/Namespace/partials/NamespacePipelineListing/index.tsx
@@ -35,16 +35,17 @@ import {
   LAST_CREATED_SORT,
   LAST_UPDATED_SORT,
   PAUSED,
-  RUNNING,
+  RUNNING, StatusString,
   STOPPED,
+  UNKNOWN,
   WARNING,
 } from "../../../../../utils";
 
 import "./style.css";
 
 const MAX_PAGE_SIZE = 4;
-export const HEALTH = [ALL, HEALTHY, WARNING, CRITICAL];
-export const STATUS = [ALL, RUNNING, STOPPED, PAUSED];
+export const HEALTH = [ALL, HEALTHY, WARNING, CRITICAL, UNKNOWN];
+export const STATUS = [ALL, RUNNING, STOPPED, PAUSED, UNKNOWN];
 
 const sortOptions = [
   {
@@ -143,11 +144,11 @@ export function NamespacePipelineListing({
     } else if (orderBy.value === LAST_UPDATED_SORT) {
       filtered.sort((a: PipelineData, b: PipelineData) => {
         if (orderBy.sortOrder === ASC) {
-          return a.pipeline.status.lastUpdated > b.pipeline.status.lastUpdated
+          return a?.pipeline?.status?.lastUpdated > b.pipeline?.status?.lastUpdated
             ? 1
             : -1;
         } else {
-          return a.pipeline.status.lastUpdated < b.pipeline.status.lastUpdated
+          return a?.pipeline?.status?.lastUpdated < b.pipeline?.status?.lastUpdated
             ? 1
             : -1;
         }
@@ -155,13 +156,13 @@ export function NamespacePipelineListing({
     } else {
       filtered.sort((a: PipelineData, b: PipelineData) => {
         if (orderBy.sortOrder === ASC) {
-          return Date.parse(a.pipeline.metadata.creationTimestamp) >
-            Date.parse(b.pipeline.metadata.creationTimestamp)
+          return Date.parse(a?.pipeline?.metadata?.creationTimestamp) >
+            Date.parse(b.pipeline?.metadata?.creationTimestamp)
             ? 1
             : -1;
         } else {
-          return Date.parse(a.pipeline.metadata.creationTimestamp) <
-            Date.parse(b.pipeline.metadata.creationTimestamp)
+          return Date.parse(a?.pipeline?.metadata?.creationTimestamp) <
+            Date.parse(b?.pipeline?.metadata?.creationTimestamp)
             ? 1
             : -1;
         }
@@ -170,7 +171,8 @@ export function NamespacePipelineListing({
     //Filter by health
     if (health !== "All") {
       filtered = filtered.filter((p) => {
-        if (p.status.toLowerCase() === health.toLowerCase()) {
+        const status = p?.status || UNKNOWN;
+        if (status.toLowerCase() === health.toLowerCase()) {
           return true;
         } else {
           return false;
@@ -181,7 +183,8 @@ export function NamespacePipelineListing({
     //Filter by status
     if (status !== "All") {
       filtered = filtered.filter((p) => {
-        if (p.pipeline.status.phase.toLowerCase() === status.toLowerCase()) {
+        const currentStatus = p?.pipeline?.status?.phase || UNKNOWN;
+        if (currentStatus.toLowerCase() === status.toLowerCase()) {
           return true;
         } else {
           return false;
@@ -239,26 +242,26 @@ export function NamespacePipelineListing({
       filtered.sort((a: ISBServicesListing, b: ISBServicesListing) => {
         if (orderBy.sortOrder === ASC) {
           return new Date(
-            a.isbService.status.conditions[
-              a.isbService.status.conditions.length - 1
+            a?.isbService?.status?.conditions[
+              a?.isbService?.status?.conditions?.length - 1
             ].lastTransitionTime
           ) >
             new Date(
-              b.isbService.status.conditions[
-                b.isbService.status.conditions.length - 1
+              b?.isbService?.status?.conditions[
+                b?.isbService?.status?.conditions?.length - 1
               ].lastTransitionTime
             )
             ? 1
             : -1;
         } else {
           return new Date(
-            a.isbService.status.conditions[
-              a.isbService.status.conditions.length - 1
+            a?.isbService?.status?.conditions[
+              a?.isbService?.status?.conditions?.length - 1
             ].lastTransitionTime
           ) <
             new Date(
-              b.isbService.status.conditions[
-                b.isbService.status.conditions.length - 1
+              b?.isbService?.status?.conditions[
+                b?.isbService?.status?.conditions?.length - 1
               ].lastTransitionTime
             )
             ? 1
@@ -268,13 +271,13 @@ export function NamespacePipelineListing({
     } else {
       filtered.sort((a: ISBServicesListing, b: ISBServicesListing) => {
         if (orderBy.sortOrder === ASC) {
-          return new Date(a.isbService.metadata.creationTimestamp) >
-            new Date(b.isbService.metadata.creationTimestamp)
+          return new Date(a?.isbService?.metadata?.creationTimestamp) >
+            new Date(b?.isbService?.metadata?.creationTimestamp)
             ? 1
             : -1;
         } else {
-          return new Date(a.isbService.metadata.creationTimestamp) <
-            new Date(b.isbService.metadata.creationTimestamp)
+          return new Date(a?.isbService?.metadata?.creationTimestamp) <
+            new Date(b?.isbService?.metadata?.creationTimestamp)
             ? 1
             : -1;
         }
@@ -283,7 +286,8 @@ export function NamespacePipelineListing({
     //Filter by health
     if (health !== "All") {
       filtered = filtered.filter((p: ISBServicesListing) => {
-        if (p.status.toLowerCase() === health.toLowerCase()) {
+        const status = p?.status || UNKNOWN;
+        if (status.toLowerCase() === health.toLowerCase()) {
           return true;
         } else {
           return false;
@@ -294,7 +298,8 @@ export function NamespacePipelineListing({
     //Filter by status
     if (status !== "All") {
       filtered = filtered.filter((p: ISBServicesListing) => {
-        if (p.isbService.status.phase.toLowerCase() === status.toLowerCase()) {
+        const currentStatus = p?.isbService?.status?.phase || UNKNOWN;
+        if (currentStatus.toLowerCase() === status.toLowerCase()) {
           return true;
         } else {
           return false;
@@ -561,7 +566,7 @@ export function NamespacePipelineListing({
                   value={status}
                   sx={{ textTransform: "capitalize" }}
                 >
-                  {status}
+                  {StatusString[status]}
                 </MenuItem>
               ))}
             </Select>
