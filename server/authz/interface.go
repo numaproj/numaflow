8ed52b39c086532bc5b8e1a53352718219c718c6
Sidhant Kohli <sidhant.kohli@gmail.com>
2023-11-01 11:25:51 -0700

    feat: add scopes to authorization (#1288)
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- server/authz/interface.go
+++ server/authz/interface.go
@@ -1,12 +1,15 @@
 package authz
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/numaproj/numaflow/server/authn"
+)
 
 type Authorizer interface {
 	// Authorize checks if a user is authorized to access the resource.
-	// c is the gin context.
-	// g is the list of groups the user belongs to.
 	// Authorize trusts that the user is already authenticated and directly uses the groups to authorize the user.
 	// please don't use gin to get the user information again.
-	Authorize(c *gin.Context, g []string) (bool, error)
+	// Authorize returns true if the user is authorized, otherwise false.
+	Authorize(c *gin.Context, userInfo *authn.UserInfo) bool
 }
