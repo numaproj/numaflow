25aa2074fa3130857830e2d77509ab2ccf28a923
Derek Wang <whynowy@gmail.com>
2023-11-03 00:12:19 -0700

    chore: controller start args in a configmap
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- cmd/commands/controller.go
+++ cmd/commands/controller.go
@@ -37,7 +37,7 @@ func NewControllerCommand() *cobra.Command {
 			ctrlcmd.Start(namespaced, managedNamespace)
 		},
 	}
-	command.Flags().BoolVar(&namespaced, "namespaced", false, "Whether to run in namespaced scope, defaults to false.")
-	command.Flags().StringVar(&managedNamespace, "managed-namespace", sharedutil.LookupEnvStringOr("NAMESPACE", "numaflow-system"), "The namespace that the controller watches when \"--namespaced\" is \"true\".")
+	command.Flags().BoolVar(&namespaced, "namespaced", sharedutil.LookupEnvBoolOr("NUMAFLOW_CONTROLLER_NAMESPACED", false), "Whether to run in namespaced scope, defaults to false.")
+	command.Flags().StringVar(&managedNamespace, "managed-namespace", sharedutil.LookupEnvStringOr("NUMAFLOW_CONTROLLER_MANAGED_NAMESPACE", sharedutil.LookupEnvStringOr("NAMESPACE", "numaflow-system")), "The namespace that the server watches when \"--namespaced\" is \"true\".")
 	return command
 }
