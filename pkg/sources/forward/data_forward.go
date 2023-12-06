a4b08235d84a7ea2677f249c9307f41f0d45c78a
Yashash H L <yashashhl25@gmail.com>
2023-12-06 11:56:38 +0530

    return early
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- pkg/sources/forward/data_forward.go
+++ pkg/sources/forward/data_forward.go
@@ -208,29 +208,33 @@ func (df *DataForward) forwardAChunk(ctx context.Context) {
 		metrics.ReadMessagesError.With(map[string]string{metrics.LabelVertex: df.vertexName, metrics.LabelPipeline: df.pipelineName, metrics.LabelVertexType: string(dfv1.VertexTypeSource), metrics.LabelVertexReplicaIndex: strconv.Itoa(int(df.vertexReplica)), metrics.LabelPartitionName: df.reader.GetName()}).Inc()
 	}
 
-	// Process only if we have any read messages, else try to publish idle-watermark and return.
-	if len(readMessages) == 0 && df.srcIdleHandler.IsSourceIdling() {
-		// publish idle watermark for the source
-		df.srcIdleHandler.PublishSourceIdleWatermark(df.reader.Partitions())
+	// if there are no read messages, we return early.
+	if len(readMessages) == 0 {
+		// if the source is idling, we will publish idle watermark to the source and all the toBuffers
+		// we will not publish idle watermark if the source is not idling.
+		if df.srcIdleHandler.IsSourceIdling() {
+			// publish idle watermark for the source
+			df.srcIdleHandler.PublishSourceIdleWatermark(df.reader.Partitions())
 
-		// if we have published idle watermark to source, we need to publish idle watermark to all the toBuffers
-		// it might not get the latest watermark because of publishing delay, but we will get in the subsequent
-		// iterations.
+			// if we have published idle watermark to source, we need to publish idle watermark to all the toBuffers
+			// it might not get the latest watermark because of publishing delay, but we will get in the subsequent
+			// iterations.
 
-		// publish idle watermark for all the toBuffers
-		fetchedWm := df.wmFetcher.ComputeWatermark()
-		for toVertexName, toVertexBuffers := range df.toBuffers {
-			for index := range toVertexBuffers {
-				// publish idle watermark to all the source partitions owned by this reader.
-				// it is 1:1 for many (HTTP, tickgen, etc.) but for e.g., for Kafka it is 1:N and the list of partitions in the N could keep changing.
-				for _, sp := range df.reader.Partitions() {
-					if vertexPublishers, ok := df.toVertexWMPublishers[toVertexName]; ok {
-						var publisher, ok = vertexPublishers[sp]
-						if !ok {
-							publisher = df.createToVertexWatermarkPublisher(toVertexName, sp)
-							vertexPublishers[sp] = publisher
+			// publish idle watermark for all the toBuffers
+			fetchedWm := df.wmFetcher.ComputeWatermark()
+			for toVertexName, toVertexBuffers := range df.toBuffers {
+				for index := range toVertexBuffers {
+					// publish idle watermark to all the source partitions owned by this reader.
+					// it is 1:1 for many (HTTP, tickgen, etc.) but for e.g., for Kafka it is 1:N and the list of partitions in the N could keep changing.
+					for _, sp := range df.reader.Partitions() {
+						if vertexPublishers, ok := df.toVertexWMPublishers[toVertexName]; ok {
+							var publisher, ok = vertexPublishers[sp]
+							if !ok {
+								publisher = df.createToVertexWatermarkPublisher(toVertexName, sp)
+								vertexPublishers[sp] = publisher
+							}
+							idlehandler.PublishIdleWatermark(ctx, df.toBuffers[toVertexName][index], publisher, df.idleManager, df.opts.logger, dfv1.VertexTypeSource, fetchedWm)
 						}
-						idlehandler.PublishIdleWatermark(ctx, df.toBuffers[toVertexName][index], publisher, df.idleManager, df.opts.logger, dfv1.VertexTypeSource, fetchedWm)
 					}
 				}
 			}
@@ -424,6 +428,10 @@ func (df *DataForward) forwardAChunk(ctx context.Context) {
 }
 
 func (df *DataForward) ackFromSource(ctx context.Context, offsets []isb.Offset) error {
+	// if there are no offsets, we return early.
+	if len(offsets) == 0 {
+		return nil
+	}
 	// for all the sources, we either ack all offsets or none.
 	// when a batch ack fails, the source Ack() function populate the error array with the same error;
 	// hence we can just return the first error.
