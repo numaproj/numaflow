f4ef43868bafa44f6ed08e9c29b200872546b58a
Yashash H L <yashashhl25@gmail.com>
2024-02-13 08:21:44 +0530

    update test
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- pkg/reduce/pbq/store/unaligned/store_benchmark_test.go
+++ pkg/reduce/pbq/store/unaligned/store_benchmark_test.go
@@ -7,34 +7,34 @@ import (
 	"testing"
 )
 
-const output = "this is just an example string"
+const output = "this is just an example string%\n"
 
 func BenchmarkBufioWriter(b *testing.B) {
 	file, _ := os.Create("test.txt")
-	defer file.Close()
+	defer func(file *os.File) {
+		_ = file.Close()
+		_ = os.Remove("test.txt")
+	}(file)
 	buffer := bufio.NewWriter(file)
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		buffer.Write([]byte(output + fmt.Sprintf("%d\n", i)))
-		buffer.Flush()
+		_, _ = buffer.Write([]byte(output + fmt.Sprintf("%d\n", i)))
 	}
+	_ = buffer.Flush()
 }
-
-// BenchmarkBufioWriter-10    	  752877	      1618 ns/op	      48 B/op	       1 allocs/op - message - "yashash"
-// BenchmarkBufioWriter-10    	  677716	      1781 ns/op	     126 B/op	       3 allocs/op - message - "yashash - %d\n"
 
 func BenchmarkFileWriteAt(b *testing.B) {
 	file, _ := os.Create("test.txt")
-	defer file.Close()
+	defer func(file *os.File) {
+		_ = file.Close()
+		_ = os.Remove("test.txt")
+	}(file)
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		file.WriteAt([]byte(output+fmt.Sprintf("%d\n", i)), int64(i))
+		_, _ = file.WriteAt([]byte(output+fmt.Sprintf("%d\n", i)), int64(i))
 	}
 }
-
-// BenchmarkFileWriteAt-10    	  636252	      1646 ns/op	       0 B/op	       0 allocs/op - message - "yashash"
-// BenchmarkFileWriteAt-10    	  675021	      1686 ns/op	     126 B/op	       3 allocs/op - message - "yashash - %d\n"
