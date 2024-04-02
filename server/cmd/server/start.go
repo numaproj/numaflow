d47e5471c00807dca589c88564d462d60d453df4
Ali Ibrahim <ali_ibrahim1@intuit.com>
2024-04-02 13:44:54 -0700

    change AllowAllOrigins true to from confing
    Signed-off-by: Ali Ibrahim <ali_ibrahim1@intuit.com>

--- server/cmd/server/start.go
+++ server/cmd/server/start.go
@@ -70,7 +70,7 @@ func (s *server) Start(ctx context.Context) {
 	router := gin.New()
 	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{SkipPaths: []string{"/livez"}}))
 	router.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
+		AllowOrigins:     s.options.CorsAllowedOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
 		AllowCredentials: true,
