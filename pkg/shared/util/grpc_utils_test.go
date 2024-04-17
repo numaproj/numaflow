6592bcf0dce432665526216a189c19a2bbdbff13
a3hadi <a3hadi@protonmail.com>
2024-04-17 19:56:05 -0400

    fix unit tests
    Signed-off-by: a3hadi <a3hadi@protonmail.com>

--- pkg/shared/util/grpc_utils_test.go
+++ pkg/shared/util/grpc_utils_test.go
@@ -7,27 +7,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testVersionMappingConfig = map[string]sdkConstraints{
-	"1.1.7": {
-		info.Go:     "0.5.0-0",
-		info.Python: "0.5.0a",
-		info.Java:   "0.5.0-0",
-	},
-	"1.1.8": {
-		info.Go:     "0.6.0-0",
-		info.Python: "0.6.0a",
-		info.Java:   "0.6.0-0",
-	},
+var testMinimumSupportedSDKVersions = sdkConstraints{
+	info.Go:     "0.6.0-0",
+	info.Python: "0.6.0a",
+	info.Java:   "0.6.0-0",
 }
 
 func TestCheckCompatibility(t *testing.T) {
 	tests := []struct {
-		name                 string
-		serverInfo           *info.ServerInfo
-		versionMappingConfig map[string]sdkConstraints
-		numaflowVersion      string
-		shouldErr            bool
-		errMessage           string
+		name                        string
+		serverInfo                  *info.ServerInfo
+		minimumSupportedSDKVersions sdkConstraints
+		numaflowVersion             string
+		shouldErr                   bool
+		errMessage                  string
 	}{
 		{
 			name: "Test with incompatible SDK version",
@@ -38,10 +31,10 @@ func TestCheckCompatibility(t *testing.T) {
 				Version:                "0.5.3",
 				Metadata:               nil,
 			},
-			versionMappingConfig: testVersionMappingConfig,
-			numaflowVersion:      "1.1.8",
-			shouldErr:            true,
-			errMessage:           "SDK version 0.5.3 must be upgraded to 0.6.0a or later, in order to work with current numaflow version 1.1.8",
+			minimumSupportedSDKVersions: testMinimumSupportedSDKVersions,
+			numaflowVersion:             "1.1.8",
+			shouldErr:                   true,
+			errMessage:                  "SDK version 0.5.3 must be upgraded to 0.6.0a or later, in order to work with current numaflow version 1.1.8",
 		},
 		{
 			name: "Test with incompatible numaflow version",
@@ -52,10 +45,10 @@ func TestCheckCompatibility(t *testing.T) {
 				Version:                "0.7.0-rc1",
 				Metadata:               nil,
 			},
-			versionMappingConfig: testVersionMappingConfig,
-			numaflowVersion:      "1.1.7",
-			shouldErr:            true,
-			errMessage:           "numaflow version 1.1.7 must be upgraded to 1.1.8-0 or later, in order to work with current SDK version 0.7.0-rc1",
+			minimumSupportedSDKVersions: testMinimumSupportedSDKVersions,
+			numaflowVersion:             "1.1.7",
+			shouldErr:                   true,
+			errMessage:                  "numaflow version 1.1.7 must be upgraded to 1.1.8-0 or later, in order to work with current SDK version 0.7.0-rc1",
 		},
 		{
 			name: "Test with compatible numaflow and SDK version",
@@ -66,15 +59,15 @@ func TestCheckCompatibility(t *testing.T) {
 				Version:                "0.5.0-rc2",
 				Metadata:               nil,
 			},
-			versionMappingConfig: testVersionMappingConfig,
-			numaflowVersion:      "1.1.7",
-			shouldErr:            false,
+			minimumSupportedSDKVersions: testMinimumSupportedSDKVersions,
+			numaflowVersion:             "1.1.7",
+			shouldErr:                   false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := checkCompatibility(tt.serverInfo, tt.versionMappingConfig, tt.numaflowVersion)
+			err := checkCompatibility(tt.serverInfo, tt.minimumSupportedSDKVersions, tt.numaflowVersion)
 			if tt.shouldErr {
 				assert.Error(t, err, "Expected error")
 				assert.Contains(t, err.Error(), tt.errMessage)
