5f5644d6b2d79cdcaccbcf7214fcfa1acb373efd
Samhith Kakarla <samhith_kakarla@intuit.com>
2024-06-28 15:14:00 -0700

    fixing publisher
    Signed-off-by: Samhith Kakarla <samhith_kakarla@intuit.com>

--- pkg/watermark/publish/publisher_test.go
+++ pkg/watermark/publish/publisher_test.go
@@ -17,7 +17,19 @@ limitations under the License.
 package publish
 
 import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
 	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/numaproj/numaflow/pkg/isb"
+	natstest "github.com/numaproj/numaflow/pkg/shared/clients/nats/test"
+	"github.com/numaproj/numaflow/pkg/watermark/entity"
+	"github.com/numaproj/numaflow/pkg/watermark/store"
+	"github.com/numaproj/numaflow/pkg/watermark/wmb"
 )
 
 func createAndLaterDeleteBucket(js nats.JetStreamContext, kvConfig *nats.KeyValueConfig) (func(), error) {
@@ -31,71 +43,71 @@ func createAndLaterDeleteBucket(js nats.JetStreamContext, kvConfig *nats.KeyValu
 	}, nil
 }
 
-// func TestPublisherWithSharedOTBucket(t *testing.T) {
-// 	s := natstest.RunJetStreamServer(t)
-// 	defer natstest.ShutdownJetStreamServer(t, s)
+func TestPublisherWithSharedOTBucket(t *testing.T) {
+	s := natstest.RunJetStreamServer(t)
+	defer natstest.ShutdownJetStreamServer(t, s)
 
-// 	var ctx = context.Background()
+	var ctx = context.Background()
 
-// 	defaultJetStreamClient := natstest.JetStreamClient(t, s)
-// 	defer defaultJetStreamClient.Close()
-// 	js, err := defaultJetStreamClient.JetStreamContext()
-// 	assert.NoError(t, err)
+	defaultJetStreamClient := natstest.JetStreamClient(t, s)
+	defer defaultJetStreamClient.Close()
+	js, err := defaultJetStreamClient.JetStreamContext()
+	assert.NoError(t, err)
 
-// 	var keyspace = "publisherTest"
+	var keyspace = "publisherTest"
 
-// 	deleteFn, err := createAndLaterDeleteBucket(js, &nats.KeyValueConfig{Bucket: keyspace + "_PROCESSORS"})
-// 	assert.NoError(t, err)
-// 	defer deleteFn()
+	deleteFn, err := createAndLaterDeleteBucket(js, &nats.KeyValueConfig{Bucket: keyspace + "_PROCESSORS"})
+	assert.NoError(t, err)
+	defer deleteFn()
 
-// 	deleteFn, err = createAndLaterDeleteBucket(js, &nats.KeyValueConfig{Bucket: keyspace + "_OT"})
-// 	assert.NoError(t, err)
-// 	defer deleteFn()
+	deleteFn, err = createAndLaterDeleteBucket(js, &nats.KeyValueConfig{Bucket: keyspace + "_OT"})
+	assert.NoError(t, err)
+	defer deleteFn()
 
-// 	publishEntity := entity.NewProcessorEntity("publisherTestPod1")
+	publishEntity := entity.NewProcessorEntity("publisherTestPod1")
 
-// 	wmstore, err := store.BuildJetStreamWatermarkStore(ctx, keyspace, defaultJetStreamClient)
-// 	assert.NoError(t, err)
+	wmstore, err := store.BuildJetStreamWatermarkStore(ctx, keyspace, defaultJetStreamClient)
+	assert.NoError(t, err)
 
-// 	p := NewPublish(ctx, publishEntity, wmstore, 1, WithAutoRefreshHeartbeatDisabled(), WithPodHeartbeatRate(1)).(*publish)
+	p := NewPublish(ctx, publishEntity, wmstore, 1, WithAutoRefreshHeartbeatDisabled(), WithPodHeartbeatRate(1)).(*publish)
 
-// 	var epoch int64 = 1651161600000
-// 	var location, _ = time.LoadLocation("UTC")
-// 	for i := 0; i < 3; i++ {
-// 		p.PublishWatermark(wmb.Watermark(time.UnixMilli(epoch).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(i) }), 0)
-// 		epoch += 60000
-// 		time.Sleep(time.Millisecond)
-// 	}
-// 	// publish a stale watermark (offset doesn't matter)
-// 	p.PublishWatermark(wmb.Watermark(time.UnixMilli(epoch-120000).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(0) }), 0)
+	var epoch int64 = 1651161600000
+	var location, _ = time.LoadLocation("UTC")
+	for i := 0; i < 3; i++ {
+		p.PublishWatermark(wmb.Watermark(time.UnixMilli(epoch).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(i) }), 0)
+		epoch += 60000
+		time.Sleep(time.Millisecond)
+	}
+	// publish a stale watermark (offset doesn't matter)
+	p.PublishWatermark(wmb.Watermark(time.UnixMilli(epoch-120000).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(0) }), 0)
 
-// 	keys, err := p.otStore.GetAllKeys(p.ctx)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, []string{"publisherTestPod1"}, keys)
+	keys, err := p.otStore.GetAllKeys(p.ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"publisherTestPod1"}, keys)
 
-// 	wm := p.loadLatestFromStore()
-// 	assert.Equal(t, wmb.Watermark(time.UnixMilli(epoch-60000).In(location)).String(), wm.String())
+	wm := p.loadLatestFromStore()
+	assert.Equal(t, wmb.Watermark(time.UnixMilli(epoch-60000).In(location)).String(), wm.String())
 
-// 	head := p.GetLatestWatermark()
-// 	assert.Equal(t, wmb.Watermark(time.UnixMilli(epoch-60000).In(location)).String(), head.String())
+	head := p.GetLatestWatermark()
+	assert.Equal(t, wmb.Watermark(time.UnixMilli(epoch-60000).In(location)).String(), head.String())
 
-// 	// simulate the next read batch where the watermark is now $epoch
-// 	// set offset to -1 to simulate reduce vertex publish idle watermark
-// 	p.PublishIdleWatermark(wmb.Watermark(time.UnixMilli(epoch).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(-1) }), 0)
-// 	keys, err = p.otStore.GetAllKeys(p.ctx)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, []string{"publisherTestPod1"}, keys)
-// 	otValue, err := p.otStore.GetValue(p.ctx, keys[0])
-// 	assert.NoError(t, err)
-// 	otDecode, err := wmb.DecodeToWMB(otValue)
-// 	assert.NoError(t, err)
-// 	assert.True(t, otDecode.Idle)
-// 	assert.Equal(t, otDecode.Watermark, epoch)
-// 	assert.Equal(t, otDecode.Offset, int64(-1))
-// 	assert.Equal(t, otDecode.Partition, int32(0))
+	// simulate the next read batch where the watermark is now $epoch
+	// set offset to -1 to simulate reduce vertex publish idle watermark
+	p.PublishIdleWatermark(wmb.Watermark(time.UnixMilli(epoch).In(location)), isb.SimpleStringOffset(func() string { return strconv.Itoa(-1) }), 0)
+	keys, err = p.otStore.GetAllKeys(p.ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"publisherTestPod1"}, keys)
+	otValue, err := p.otStore.GetValue(p.ctx, keys[0])
+	assert.NoError(t, err)
+	otDecode, err := wmb.DecodeToWMB(otValue)
+	assert.NoError(t, err)
+	assert.True(t, otDecode.Idle)
+	assert.Equal(t, otDecode.Watermark, epoch)
+	assert.Equal(t, otDecode.Offset, int64(-1))
+	assert.Equal(t, otDecode.Partition, int32(0))
 
-// 	_ = p.Close()
+	_ = p.Close()
 
-// 	_, err = p.heartbeatStore.GetValue(ctx, publishEntity.GetName())
-// 	assert.Equal(t, nats.ErrKeyNotFound, err)
-// }
+	_, err = p.heartbeatStore.GetValue(ctx, publishEntity.GetName())
+	assert.Equal(t, nats.ErrKeyNotFound, err)
+}
