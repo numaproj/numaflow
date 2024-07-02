1679493ed9ffb96dd624626dc091aa93962bb70b
Sidhant Kohli <sidhant.kohli@gmail.com>
2024-07-01 17:28:06 -0700

    refactor
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- pkg/sdkclient/batchmapper/client_test.go
+++ pkg/sdkclient/batchmapper/client_test.go
@@ -30,6 +30,8 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	sdkerr "github.com/numaproj/numaflow/pkg/sdkclient/error"
 )
 
 func TestClient_IsReady(t *testing.T) {
@@ -114,6 +116,83 @@ func TestClient_BatchMapFn(t *testing.T) {
 	}
 }
 
+func TestClientStreamError_BatchMapFn(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mapmock.NewMockMapClient(ctrl)
+	mockMapclient := mapmock.NewMockMap_MapStreamFnClient(ctrl)
+
+	mockMapclient.EXPECT().Send(gomock.Any()).Return(nil).AnyTimes()
+	mockMapclient.EXPECT().CloseSend().Return(nil).AnyTimes()
+	mockMapclient.EXPECT().Recv().Return(&mappb.MapResponse{
+		Results: []*mappb.MapResponse_Result{
+			{
+				Keys:  []string{"client_test"},
+				Value: []byte(`test1`),
+			},
+		},
+		Id: "test1",
+	}, fmt.Errorf("recv error")).AnyTimes()
+	mockMapclient.EXPECT().Recv().Return(&mappb.MapResponse{
+		Results: []*mappb.MapResponse_Result{
+			{
+				Keys:  []string{"client_test"},
+				Value: []byte(`test2`),
+			},
+		},
+		Id: "test2",
+	}, io.EOF).AnyTimes()
+
+	mockClient.EXPECT().MapStreamFn(gomock.Any(), gomock.Any()).Return(mockMapclient, nil)
+
+	testClient, err := NewFromClient(mockClient)
+	assert.NoError(t, err)
+	reflect.DeepEqual(testClient, &client{
+		grpcClt: mockClient,
+	})
+
+	messageCh := make(chan *mappb.MapRequest)
+	responseCh, errCh := testClient.BatchMapFn(ctx, messageCh)
+	go func() {
+		defer close(messageCh)
+		requests := []*mappb.MapRequest{{
+			Keys:      []string{"client"},
+			Value:     []byte(`test1`),
+			EventTime: timestamppb.New(time.Time{}),
+			Watermark: timestamppb.New(time.Time{}),
+			Id:        "test1",
+		}, {
+			Keys:      []string{"client"},
+			Value:     []byte(`test2`),
+			EventTime: timestamppb.New(time.Time{}),
+			Watermark: timestamppb.New(time.Time{}),
+			Id:        "test2",
+		}}
+		for _, req := range requests {
+			messageCh <- req
+		}
+	}()
+	//var caughtContextError error
+readLoop:
+	for {
+		select {
+		case err := <-errCh:
+			if err != nil {
+				assert.Equal(t, err, sdkerr.ToUDFErr("c.grpcClt.BatchMapFn", fmt.Errorf("recv error")))
+				break readLoop
+			}
+		case _, ok := <-responseCh:
+			if !ok {
+				break
+			}
+		}
+	}
+}
+
 func TestClientContextClosed_BatchMapFn(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
@@ -173,25 +252,23 @@ func TestClientContextClosed_BatchMapFn(t *testing.T) {
 			messageCh <- req
 		}
 	}()
-
-	//idx := 1
-	caughtContextError := false
+	var caughtError error
 readLoop:
 	for {
 		select {
 		case err := <-errCh:
 			if err != nil {
-				caughtContextError = true
-				assert.Error(t, err, ctx.Err())
+				caughtError = err
+				assert.Equal(t, err, ctx.Err())
 				break readLoop
 			}
 		case _, ok := <-responseCh:
 			if !ok {
-
+				break
 			}
 		}
 		// explicitly cancel the context
 		cancel()
 	}
-	assert.True(t, caughtContextError)
+	assert.Equal(t, caughtError, ctx.Err())
 }
