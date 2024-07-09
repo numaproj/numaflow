05efa4addff1507ef08c27c316196598524d0355
Sidhant Kohli <sidhant.kohli@gmail.com>
2024-07-09 15:58:49 -0700

    refactor
    Signed-off-by: Sidhant Kohli <sidhant.kohli@gmail.com>

--- pkg/udf/rpc/grpc_batch_map_test.go
+++ pkg/udf/rpc/grpc_batch_map_test.go
@@ -81,7 +81,6 @@ func TestGRPCBasedBatchMap_BasicBatchMapFnWithMockClient(t *testing.T) {
 		}()
 
 		u := NewMockUDSGRPCBasedBatchMap(mockClient)
-		//readMessages := testutils.BuildTestReadMessages(2, time.Unix(1661169600, 0), nil)
 
 		dataMessages := make([]*isb.ReadMessage, 0)
 
@@ -94,27 +93,5 @@ func TestGRPCBasedBatchMap_BasicBatchMapFnWithMockClient(t *testing.T) {
 				MainCarDown: false,
 			},
 		})
-	})
-
-	t.Run("test context close", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		mockClient := batchmapmock.NewMockBatchMapClient(ctrl)
-		mockBatchMapClient := batchmapmock.NewMockBatchMap_BatchMapFnClient(ctrl)
-		mockBatchMapClient.EXPECT().Send(gomock.Any()).Return(nil).AnyTimes()
-		mockBatchMapClient.EXPECT().CloseSend().Return(nil).AnyTimes()
-		mockClient.EXPECT().BatchMapFn(gomock.Any(), gomock.Any()).Return(mockBatchMapClient, nil)
-		mockBatchMapClient.EXPECT().Recv().Return(nil, errors.New("mock error for map")).Times(1)
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-
-		u := NewMockUDSGRPCBasedBatchMap(mockClient)
-
-		dataMessages := make([]*isb.ReadMessage, 0)
-		// explicit cancel the context, we should see that error
-		cancel()
-		_, err := u.ApplyBatchMap(ctx, dataMessages)
-		assert.Error(t, err, ctx.Err())
 	})
 }
