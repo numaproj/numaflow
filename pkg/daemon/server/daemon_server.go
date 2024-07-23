6503e40a0ce1bf379616274ad309cd31ec4a11aa
Samhith Kakarla <samhith_kakarla@intuit.com>
2024-07-23 15:18:25 -0700

    fixes2
    Signed-off-by: Samhith Kakarla <samhith_kakarla@intuit.com>

--- pkg/daemon/server/daemon_server.go
+++ pkg/daemon/server/daemon_server.go
@@ -306,8 +306,13 @@ func (ds *daemonServer) exposeMetrics(ctx context.Context) {
 				watermarkCmpNow.WithLabelValues(ds.pipeline.Name).Set(float64(time.Now().UnixMilli() - maxWM))
 			}
 
-			temp := ds.metaDataQuery.GetHealthCheck(ctx)
-			switch temp.Status {
+			temp, err := ds.metaDataQuery.GetPipelineStatus(ctx, &daemon.GetPipelineStatusRequest{Pipeline: ds.pipeline.Name})
+
+			if err != nil {
+				log.Errorw("Failed to get data processing health status", zap.Error(err))
+				continue
+			}
+			switch temp.Status.Status {
 			case v1alpha1.PipelineStatusHealthy:
 				dataProcessingHealth.WithLabelValues(ds.pipeline.Name).Set(1)
 			case v1alpha1.PipelineStatusWarning:
