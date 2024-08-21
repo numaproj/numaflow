9cbb039faadfb444a889b319f7bf1440d9368e56
Keran Yang <yangkr920208@gmail.com>
2024-08-21 13:12:16 -0400

    move controller stream place
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- test/fixtures/e2e_suite.go
+++ test/fixtures/e2e_suite.go
@@ -112,6 +112,8 @@ func (s *E2ESuite) SetupSuite() {
 		Expect().
 		ISBSvcDeleted(defaultTimeout)
 
+	s.Given().When().StreamControllerLogs()
+
 	s.Given().ISBSvc(getISBSvcSpec()).
 		When().
 		CreateISBSvc().
@@ -119,7 +121,6 @@ func (s *E2ESuite) SetupSuite() {
 	s.T().Log("ISB svc is ready")
 
 	s.Given().When().StreamISBLogs("main")
-	s.Given().When().StreamControllerLogs()
 
 	err = PodPortForward(s.restConfig, Namespace, "e2e-api-pod", 8378, 8378, s.stopch)
 	s.CheckError(err)
