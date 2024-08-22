04be68876e208d76130089e02478af10064a29bd
Yashash H L <yashashhl25@gmail.com>
2024-08-22 08:56:48 +0530

    chore: metrics and https support for serving (#1985)
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>
    Signed-off-by: Vigith Maurice <vigith@gmail.com>
    Co-authored-by: Vigith Maurice <vigith@gmail.com>

--- rust/serving/src/metrics.rs
+++ rust/serving/src/metrics.rs
@@ -1,5 +1,7 @@
-use std::{future::ready, time::Instant};
-
+use crate::Error::MetricsServer;
+use axum::body::Body;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::{
     extract::{MatchedPath, Request},
     middleware::Next,
@@ -7,52 +9,111 @@ use axum::{
     routing::get,
     Router,
 };
-use metrics::{counter, histogram};
-use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
-use tokio::net::{TcpListener, ToSocketAddrs};
+use axum_server::tls_rustls::RustlsConfig;
+use prometheus_client::encoding::text::encode;
+use prometheus_client::metrics::counter::Counter;
+use prometheus_client::metrics::family::Family;
+use prometheus_client::metrics::histogram::{exponential_buckets, Histogram};
+use prometheus_client::registry::Registry;
+use std::net::SocketAddr;
+use std::sync::OnceLock;
+use std::time::Instant;
 use tracing::debug;
 
-/// Collect and emit prometheus metrics.
+// Define the labels for the metrics
+pub const SERVING_METHOD_LABEL: &str = "method";
+pub const SERVING_PATH_LABEL: &str = "path";
+const SERVING_STATUS_LABEL: &str = "status";
 
-/// Metrics router and server
-pub(crate) async fn start_metrics_server<A>(addr: A) -> crate::Result<()>
-where
-    A: ToSocketAddrs + std::fmt::Debug,
-{
-    // setup_metrics_recorder should only be invoked once
-    let recorder_handle = setup_metrics_recorder()?;
-    let metrics_app = Router::new().route("/metrics", get(move || ready(recorder_handle.render())));
+// Define the metrics
+const HTTP_REQUESTS_TOTAL: &str = "http_requests_total";
+const HTTP_REQUESTS_DURATION: &str = "http_requests_duration";
 
-    let listener = TcpListener::bind(&addr)
+#[derive(Default)]
+pub struct GlobalRegistry {
+    pub registry: parking_lot::Mutex<Registry>,
+}
+
+impl GlobalRegistry {
+    fn new() -> Self {
+        GlobalRegistry {
+            registry: parking_lot::Mutex::new(Registry::default()),
+        }
+    }
+}
+
+static GLOBAL_REGISTER: OnceLock<GlobalRegistry> = OnceLock::new();
+
+fn global_registry() -> &'static GlobalRegistry {
+    GLOBAL_REGISTER.get_or_init(GlobalRegistry::new)
+}
+
+pub struct ServingMetrics {
+    pub http_requests_total: Family<Vec<(String, String)>, Counter>,
+    pub http_requests_duration: Family<Vec<(String, String)>, Histogram>,
+}
+
+impl ServingMetrics {
+    fn new() -> Self {
+        let http_requests_total = Family::<Vec<(String, String)>, Counter>::default();
+        let http_requests_duration =
+            Family::<Vec<(String, String)>, Histogram>::new_with_constructor(|| {
+                Histogram::new(exponential_buckets(0.001, 2.0, 20))
+            });
+
+        let metrics = Self {
+            http_requests_total,
+            http_requests_duration,
+        };
+
+        let mut registry = global_registry().registry.lock();
+
+        registry.register(
+            HTTP_REQUESTS_TOTAL,
+            "A Counter to keep track of the total number of HTTP requests",
+            metrics.http_requests_total.clone(),
+        );
+        registry.register(
+            HTTP_REQUESTS_DURATION,
+            "A Histogram to keep track of the duration of HTTP requests",
+            metrics.http_requests_duration.clone(),
+        );
+
+        metrics
+    }
+}
+
+static SERVING_METRICS: OnceLock<ServingMetrics> = OnceLock::new();
+
+pub(crate) fn serving_metrics() -> &'static ServingMetrics {
+    SERVING_METRICS.get_or_init(ServingMetrics::new)
+}
+
+pub(crate) async fn start_https_metrics_server(
+    addr: SocketAddr,
+    tls_config: RustlsConfig,
+) -> crate::Result<()> {
+    let metrics_app = Router::new().route("/metrics", get(metrics_handler));
+
+    axum_server::bind_rustls(addr, tls_config)
+        .serve(metrics_app.into_make_service())
         .await
-        .map_err(|e| format!("Creating listener on {:?}: {}", addr, e))?;
+        .map_err(|e| MetricsServer(format!("Starting web server for metrics: {}", e)))?;
 
-    debug!("metrics server started at addr: {:?}", addr);
-
-    axum::serve(listener, metrics_app)
-        .await
-        .map_err(|e| format!("Starting web server for metrics: {}", e))?;
     Ok(())
 }
 
-/// setup the Prometheus metrics recorder.
-fn setup_metrics_recorder() -> crate::Result<PrometheusHandle> {
-    // 1 micro-sec < t < 1000 seconds
-    let log_to_power_of_sqrt2_bins: [f64; 62] = (0..62)
-        .map(|i| 2_f64.sqrt().powf(i as f64))
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-
-    let prometheus_handle = PrometheusBuilder::new()
-        .set_buckets_for_metric(
-            Matcher::Full("http_requests_duration_micros".to_string()),
-            &log_to_power_of_sqrt2_bins,
-        )
-        .map_err(|e| format!("Prometheus set_buckets_for_metric: {}", e))?
-        .install_recorder()
-        .map_err(|e| format!("Prometheus install_recorder: {}", e))?;
-    Ok(prometheus_handle)
+// metrics_handler is used to generate and return a snapshot of the
+// current state of the metrics in the global registry
+pub async fn metrics_handler() -> impl IntoResponse {
+    let state = global_registry().registry.lock();
+    let mut buffer = String::new();
+    encode(&mut buffer, &state).unwrap();
+    debug!("Exposing Metrics: {:?}", buffer);
+    axum::http::Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(buffer))
+        .unwrap()
 }
 
 /// Emit request metrics and also latency metrics.
@@ -73,11 +134,21 @@ pub(crate) async fn capture_metrics(request: Request, next: Next) -> Response {
     let latency = start.elapsed().as_micros() as f64;
     let status = response.status().as_u16().to_string();
 
-    let labels = [("method", method), ("path", path), ("status", status)];
+    let labels = vec![
+        (SERVING_METHOD_LABEL.to_string(), method),
+        (SERVING_PATH_LABEL.to_string(), path),
+        (SERVING_STATUS_LABEL.to_string(), status),
+    ];
 
-    histogram!("http_requests_duration_micros", &labels).record(latency);
+    serving_metrics()
+        .http_requests_duration
+        .get_or_create(&labels)
+        .observe(latency);
 
-    counter!("http_requests_total", &labels).increment(1);
+    serving_metrics()
+        .http_requests_total
+        .get_or_create(&labels)
+        .inc();
 
     response
 }
@@ -87,19 +158,25 @@ mod tests {
     use std::net::SocketAddr;
     use std::time::Duration;
 
+    use super::*;
+    use crate::config::cert_key_pair;
     use axum::body::Body;
     use axum::http::{HeaderMap, StatusCode};
     use axum::middleware;
     use tokio::time::sleep;
     use tower::ServiceExt;
 
-    use super::*;
-
     #[tokio::test]
     async fn test_start_metrics_server() {
+        let (cert, key) = cert_key_pair();
+
+        let tls_config = RustlsConfig::from_pem(cert.pem().into(), key.serialize_pem().into())
+            .await
+            .unwrap();
+
         let addr = SocketAddr::from(([127, 0, 0, 1], 0));
         let server = tokio::spawn(async move {
-            let result = start_metrics_server(addr).await;
+            let result = start_https_metrics_server(addr, tls_config).await;
             assert!(result.is_ok())
         });
 
