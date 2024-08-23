d841421f7d09da448cae10a45fa91a3bf9013d5c
Keran Yang <yangkr920208@gmail.com>
2024-08-23 19:14:20 -0400

    fix: e2e testing isbsvc deletion timeout issue (#1997)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- test/fixtures/when.go
+++ test/fixtures/when.go
@@ -276,7 +276,7 @@ func (w *When) TerminateAllPodPortForwards() *When {
 	return w
 }
 
-func (w *When) StreamVertexPodlogs(vertexName, containerName string) *When {
+func (w *When) StreamVertexPodLogs(vertexName, containerName string) *When {
 	w.t.Helper()
 	ctx := context.Background()
 	labelSelector := fmt.Sprintf("%s=%s,%s=%s", dfv1.KeyPipelineName, w.pipeline.Name, dfv1.KeyVertexName, vertexName)
@@ -291,6 +291,44 @@ func (w *When) StreamVertexPodlogs(vertexName, containerName string) *When {
 			w.streamLogsStopChannels = make(map[string]chan struct{})
 		}
 		w.streamLogsStopChannels[pod.Name+":"+containerName] = stopCh
+	}
+	return w
+}
+
+func (w *When) StreamISBLogs() *When {
+	w.t.Helper()
+	ctx := context.Background()
+	labelSelector := fmt.Sprintf("%s=%s,%s=%s", dfv1.KeyComponent, dfv1.ComponentISBSvc, dfv1.KeyManagedBy, dfv1.ControllerISBSvc)
+	podList, err := w.kubeClient.CoreV1().Pods(Namespace).List(ctx, metav1.ListOptions{LabelSelector: labelSelector, FieldSelector: "status.phase=Running"})
+	if err != nil {
+		w.t.Fatalf("Error getting ISB service pods: %v", err)
+	}
+	for _, pod := range podList.Items {
+		stopCh := make(chan struct{}, 1)
+		streamPodLogs(ctx, w.kubeClient, Namespace, pod.Name, "main", stopCh)
+		if w.streamLogsStopChannels == nil {
+			w.streamLogsStopChannels = make(map[string]chan struct{})
+		}
+		w.streamLogsStopChannels[pod.Name+":main"] = stopCh
+	}
+	return w
+}
+
+func (w *When) StreamControllerLogs() *When {
+	w.t.Helper()
+	ctx := context.Background()
+	labelSelector := fmt.Sprintf("%s=%s", dfv1.KeyComponent, dfv1.ComponentControllerManager)
+	podList, err := w.kubeClient.CoreV1().Pods(Namespace).List(ctx, metav1.ListOptions{LabelSelector: labelSelector, FieldSelector: "status.phase=Running"})
+	if err != nil {
+		w.t.Fatalf("Error getting the controller pods: %v", err)
+	}
+	for _, pod := range podList.Items {
+		stopCh := make(chan struct{}, 1)
+		streamPodLogs(ctx, w.kubeClient, Namespace, pod.Name, "controller-manager", stopCh)
+		if w.streamLogsStopChannels == nil {
+			w.streamLogsStopChannels = make(map[string]chan struct{})
+		}
+		w.streamLogsStopChannels[pod.Name+":controller-manager"] = stopCh
 	}
 	return w
 }
