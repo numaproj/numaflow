2b08e7f6edf0771c032e6412f12ec85a0d5c2385
Yashash H L <yashashhl25@gmail.com>
2024-08-30 21:52:27 +0530

    common logger for all binary
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- rust/monovertex/src/config.rs
+++ rust/monovertex/src/config.rs
@@ -3,7 +3,6 @@ use std::sync::OnceLock;
 
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
-use tracing::level_filters::LevelFilter;
 
 use numaflow_models::models::MonoVertex;
 
@@ -14,7 +13,6 @@ const ENV_GRPC_MAX_MESSAGE_SIZE: &str = "NUMAFLOW_GRPC_MAX_MESSAGE_SIZE";
 const ENV_POD_REPLICA: &str = "NUMAFLOW_REPLICA";
 const DEFAULT_GRPC_MAX_MESSAGE_SIZE: usize = 64 * 1024 * 1024; // 64 MB
 const DEFAULT_METRICS_PORT: u16 = 2469;
-const ENV_LOG_LEVEL: &str = "NUMAFLOW_DEBUG";
 const DEFAULT_LAG_CHECK_INTERVAL_IN_SECS: u16 = 5;
 const DEFAULT_LAG_REFRESH_INTERVAL_IN_SECS: u16 = 3;
 const DEFAULT_BATCH_SIZE: u64 = 500;
@@ -38,7 +36,6 @@ pub struct Settings {
     pub batch_size: u64,
     pub timeout_in_ms: u32,
     pub metrics_server_listen_port: u16,
-    pub log_level: String,
     pub grpc_max_message_size: usize,
     pub is_transformer_enabled: bool,
     pub is_fallback_enabled: bool,
@@ -56,7 +53,6 @@ impl Default for Settings {
             batch_size: DEFAULT_BATCH_SIZE,
             timeout_in_ms: DEFAULT_TIMEOUT_IN_MS,
             metrics_server_listen_port: DEFAULT_METRICS_PORT,
-            log_level: LevelFilter::INFO.to_string(),
             grpc_max_message_size: DEFAULT_GRPC_MAX_MESSAGE_SIZE,
             is_transformer_enabled: false,
             is_fallback_enabled: false,
@@ -121,9 +117,6 @@ impl Settings {
                 .fallback
                 .is_some();
         }
-
-        settings.log_level =
-            env::var(ENV_LOG_LEVEL).unwrap_or_else(|_| LevelFilter::INFO.to_string());
 
         settings.grpc_max_message_size = env::var(ENV_GRPC_MAX_MESSAGE_SIZE)
             .unwrap_or_else(|_| DEFAULT_GRPC_MAX_MESSAGE_SIZE.to_string())
