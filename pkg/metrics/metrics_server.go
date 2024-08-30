c6530d37efce9a1a7ffd153cde104180b2c0b287
Derek Wang <whynowy@gmail.com>
2024-08-29 19:42:49 -0700

    feat: introduce `readyReplicas` for Vertex and MonoVertex (#2014)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/metrics/metrics_server.go
+++ pkg/metrics/metrics_server.go
@@ -241,6 +241,14 @@ func (ms *metricsServer) Start(ctx context.Context) (func(ctx context.Context) e
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		for _, ex := range ms.healthCheckExecutors {
+			if err := ex(); err != nil {
+				log.Errorw("Failed to execute sidecar health check", zap.Error(err))
+				w.WriteHeader(http.StatusInternalServerError)
+				_, _ = w.Write([]byte(err.Error()))
+				return
+			}
+		}
 		w.WriteHeader(http.StatusNoContent)
 	})
 	mux.HandleFunc("/livez", func(w http.ResponseWriter, r *http.Request) {
