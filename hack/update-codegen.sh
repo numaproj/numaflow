3c559048223f66e0841ac2049ecafc293891f04a
Hao Hao <xdevxhao@gmail.com>
2024-09-04 14:35:07 -0700

    fix review comment
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -17,8 +17,6 @@ cd "${FAKE_REPOPATH}"
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${FAKE_REPOPATH}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-chmod +x ${CODEGEN_PKG}/*.sh
-
 subheader "running codegen"
 bash -x ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
   github.com/numaproj/numaflow/pkg/client github.com/numaproj/numaflow/pkg/apis \
