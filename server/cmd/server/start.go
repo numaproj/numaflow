403edbf5e3649fe88f9489abe97ce104132d57ef
adarsh0728 <gooneriitk@gmail.com>
2024-09-19 19:57:33 +0530

    POST request instead of GET in the routes map
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- server/cmd/server/start.go
+++ server/cmd/server/start.go
@@ -206,6 +206,6 @@ func CreateAuthRouteMap(baseHref string) authz.RouteMap {
 		"GET:" + baseHref + "api/v1/namespaces/:namespace/mono-vertices/:mono-vertex/metrics":        authz.NewRouteInfo(authz.ObjectMonoVertex, true),
 		"POST:" + baseHref + "api/v1/namespaces/:namespace/mono-vertices":                            authz.NewRouteInfo(authz.ObjectMonoVertex, true),
 		"GET:" + baseHref + "api/v1/namespaces/:namespace/mono-vertices/:mono-vertex/health":         authz.NewRouteInfo(authz.ObjectMonoVertex, true),
-		"GET:" + baseHref + "api/v1/namespaces/:namespace/pipelines/:pipeline/getMetricData":         authz.NewRouteInfo(authz.ObjectPipeline, true),
+		"POST:" + baseHref + "api/v1/namespaces/:namespace/pipelines/:pipeline/getMetricData":        authz.NewRouteInfo(authz.ObjectPipeline, true),
 	}
 }
