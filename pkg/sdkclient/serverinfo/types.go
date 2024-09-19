a672798fe88da73f6ac1f8984d1d60a5d9dd8aad
Keran Yang <yangkr920208@gmail.com>
2024-09-19 17:06:19 -0400

    fix: support version compatibility check for pre-release versions (#2069)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- pkg/sdkclient/serverinfo/types.go
+++ pkg/sdkclient/serverinfo/types.go
@@ -16,6 +16,8 @@ limitations under the License.
 
 package serverinfo
 
+import "strings"
+
 type Language string
 
 const (
@@ -27,11 +29,76 @@ const (
 
 type sdkConstraints map[Language]string
 
+/*
+minimumSupportedSDKVersions is the minimum supported version of each SDK for the current numaflow version.
+It is used to check if the SDK is compatible with the current numaflow version.
+
+NOTE: when updating it, please also update MINIMUM_SUPPORTED_SDK_VERSIONS for mono vertex at rust/monovertex/server_info.rs
+
+Python SDK versioning follows PEP 440 (https://www.python.org/dev/peps/pep-0440/).
+The other SDKs follow the semver versioning scheme (https://semver.org/).
+
+How to update this map:
+
+There are two types of releases, one is the stable release and the other is the pre-release.
+Below are the typical formats of the versioning scheme:
+
+	+------------------+-------------------------+-----------------------------+
+	|                  |          PEP 440        |            semver           |
+	+------------------+-------------------------+-----------------------------+
+	|      stable      |          0.8.0          |           0.8.0             |
+	+------------------+-------------------------+-----------------------------+
+	|   pre-release    |    0.8.0a1,             |       0.8.0-rc1,            |
+	|                  |    0.8.0b3,             |  0.8.0-0.20240913163521,    |
+	|                  |    or 0.8.0rc1          |            etc.             |
+	+------------------+-------------------------+-----------------------------+
+
+There are two cases to consider when updating the map:
+
+1. The minimum supported version is a pre-release version.
+In this case, directly put the exact pre-release version in the map.
+E.g., if the minimum supported version is "0.8.0-rc1", then put "0.8.0-rc1" for java, go, rust.
+"0.8.0b1", "0.8.0b1" for python.
+2. The minimum supported version is a stable version.
+In this case, put (almost) the largest available pre-release version of the stable version in the map.
+This is because the go semver library considers pre-releases to be invalid if the constraint range does not include pre-releases.
+Therefore, we have to put a pre-release version of the stable version in the map and choose the largest one.
+For python, we use "rc100" as the largest pre-release version. For go, rust, we use "-z" as the largest pre-release version.
+E.g., if the minimum supported version is "0.8.0", then put "0.8.0-z" for java, go, rust, "0.8.0rc100" for python.
+A constraint ">=0.8.0-z" will match any pre-release version of 0.8.0, including "0.8.0-rc1", "0.8.0-rc2", etc.
+
+More details about version comparison can be found in the PEP 440 and semver documentation.
+*/
 var minimumSupportedSDKVersions = sdkConstraints{
-	Go:     "0.8.0",
-	Python: "0.8.0",
-	Java:   "0.8.0",
-	Rust:   "0.1.0",
+	// meaning the minimum supported python SDK version is 0.8.0
+	Python: "0.8.0rc100",
+	// meaning the minimum supported go SDK version is 0.8.0
+	Go: "0.8.0-z",
+	// meaning the minimum supported java SDK version is 0.8.0
+	Java: "0.8.0-z",
+	// meaning the minimum supported rust SDK version is 0.1.0
+	Rust: "0.1.0-z",
+}
+
+// humanReadable returns the human-readable minimum supported version.
+// it's used for logging purposes.
+// it translates the version we used in the constraints to the real minimum supported version.
+// e.g., if the given version is "0.8.0rc100", human-readable version is "0.8.0".
+// if the given version is "0.8.0-z", "0.8.0".
+// if "0.8.0-rc1", "0.8.0-rc1".
+func humanReadable(ver string) string {
+	if ver == "" {
+		return ""
+	}
+	// semver
+	if strings.HasSuffix(ver, "-z") {
+		return ver[:len(ver)-2]
+	}
+	// PEP 440
+	if strings.HasSuffix(ver, "rc100") {
+		return ver[:len(ver)-5]
+	}
+	return ver
 }
 
 type Protocol string
