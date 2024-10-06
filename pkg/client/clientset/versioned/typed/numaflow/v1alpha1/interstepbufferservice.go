39dabeaebecea02e119970352a272ab6c2c1fabd
Derek Wang <whynowy@gmail.com>
2024-10-06 12:12:02 -0700

    chore: k8s lib version upgrade to 1.31 (#2127)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/client/clientset/versioned/typed/numaflow/v1alpha1/interstepbufferservice.go
+++ pkg/client/clientset/versioned/typed/numaflow/v1alpha1/interstepbufferservice.go
@@ -20,14 +20,13 @@ package v1alpha1
 
 import (
 	"context"
-	"time"
 
 	v1alpha1 "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
 	scheme "github.com/numaproj/numaflow/pkg/client/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
-	rest "k8s.io/client-go/rest"
+	gentype "k8s.io/client-go/gentype"
 )
 
 // InterStepBufferServicesGetter has a method to return a InterStepBufferServiceInterface.
@@ -40,6 +39,7 @@ type InterStepBufferServicesGetter interface {
 type InterStepBufferServiceInterface interface {
 	Create(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.CreateOptions) (*v1alpha1.InterStepBufferService, error)
 	Update(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.UpdateOptions) (*v1alpha1.InterStepBufferService, error)
+	// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
 	UpdateStatus(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.UpdateOptions) (*v1alpha1.InterStepBufferService, error)
 	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
@@ -52,144 +52,18 @@ type InterStepBufferServiceInterface interface {
 
 // interStepBufferServices implements InterStepBufferServiceInterface
 type interStepBufferServices struct {
-	client rest.Interface
-	ns     string
+	*gentype.ClientWithList[*v1alpha1.InterStepBufferService, *v1alpha1.InterStepBufferServiceList]
 }
 
 // newInterStepBufferServices returns a InterStepBufferServices
 func newInterStepBufferServices(c *NumaflowV1alpha1Client, namespace string) *interStepBufferServices {
 	return &interStepBufferServices{
-		client: c.RESTClient(),
-		ns:     namespace,
+		gentype.NewClientWithList[*v1alpha1.InterStepBufferService, *v1alpha1.InterStepBufferServiceList](
+			"interstepbufferservices",
+			c.RESTClient(),
+			scheme.ParameterCodec,
+			namespace,
+			func() *v1alpha1.InterStepBufferService { return &v1alpha1.InterStepBufferService{} },
+			func() *v1alpha1.InterStepBufferServiceList { return &v1alpha1.InterStepBufferServiceList{} }),
 	}
-}
-
-// Get takes name of the interStepBufferService, and returns the corresponding interStepBufferService object, and an error if there is any.
-func (c *interStepBufferServices) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.InterStepBufferService, err error) {
-	result = &v1alpha1.InterStepBufferService{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		Name(name).
-		VersionedParams(&options, scheme.ParameterCodec).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// List takes label and field selectors, and returns the list of InterStepBufferServices that match those selectors.
-func (c *interStepBufferServices) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.InterStepBufferServiceList, err error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	result = &v1alpha1.InterStepBufferServiceList{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Watch returns a watch.Interface that watches the requested interStepBufferServices.
-func (c *interStepBufferServices) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	opts.Watch = true
-	return c.client.Get().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Watch(ctx)
-}
-
-// Create takes the representation of a interStepBufferService and creates it.  Returns the server's representation of the interStepBufferService, and an error, if there is any.
-func (c *interStepBufferServices) Create(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.CreateOptions) (result *v1alpha1.InterStepBufferService, err error) {
-	result = &v1alpha1.InterStepBufferService{}
-	err = c.client.Post().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(interStepBufferService).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Update takes the representation of a interStepBufferService and updates it. Returns the server's representation of the interStepBufferService, and an error, if there is any.
-func (c *interStepBufferServices) Update(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.UpdateOptions) (result *v1alpha1.InterStepBufferService, err error) {
-	result = &v1alpha1.InterStepBufferService{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		Name(interStepBufferService.Name).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(interStepBufferService).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// UpdateStatus was generated because the type contains a Status member.
-// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-func (c *interStepBufferServices) UpdateStatus(ctx context.Context, interStepBufferService *v1alpha1.InterStepBufferService, opts v1.UpdateOptions) (result *v1alpha1.InterStepBufferService, err error) {
-	result = &v1alpha1.InterStepBufferService{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		Name(interStepBufferService.Name).
-		SubResource("status").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(interStepBufferService).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Delete takes name of the interStepBufferService and deletes it. Returns an error if one occurs.
-func (c *interStepBufferServices) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		Name(name).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *interStepBufferServices) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
-	var timeout time.Duration
-	if listOpts.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
-	}
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		VersionedParams(&listOpts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// Patch applies the patch and returns the patched interStepBufferService.
-func (c *interStepBufferServices) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.InterStepBufferService, err error) {
-	result = &v1alpha1.InterStepBufferService{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("interstepbufferservices").
-		Name(name).
-		SubResource(subresources...).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(data).
-		Do(ctx).
-		Into(result)
-	return
 }
