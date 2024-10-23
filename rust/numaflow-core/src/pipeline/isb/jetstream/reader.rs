3532ad77f63082ed8a759cec0aca30eac4ef2c21
Sreekanth <prsreekanth920@gmail.com>
2024-10-23 09:10:58 +0530

    chore: Unit tests for Jetstream reader (#2176)

--- rust/numaflow-core/src/pipeline/isb/jetstream/reader.rs
+++ rust/numaflow-core/src/pipeline/isb/jetstream/reader.rs
@@ -62,9 +62,20 @@ impl JetstreamReader {
             consumer,
         };
 
-        Ok(this)
+        let (messages_tx, messages_rx) = mpsc::channel(2 * config.batch_size);
+        tokio::spawn({
+            let this = this.clone();
+            async move {
+                this.start(messages_tx).await;
+            }
+        });
+        Ok((this, messages_rx))
     }
-    // 1 buffer - multiple streams
+
+    // Stop reading from Jetstream and close the sender end of the channel.
+    pub(crate) fn cancel(&self) {
+        self.ctx.cancel();
+    }
 
     // When we encounter an error, we log the error and return from the function. This drops the sender end of the channel.
     // The closing of the channel should propagate to the receiver end and the receiver should exit gracefully.
@@ -238,5 +249,116 @@ async fn start_work_in_progress(
                 return;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use async_nats::jetstream;
+    use async_nats::jetstream::{consumer, stream};
+    use bytes::BytesMut;
+    use chrono::Utc;
+    use std::collections::HashMap;
+
+    use super::*;
+    use crate::message::{Message, MessageID, Offset};
+    use crate::pipeline::isb::jetstream::writer::JetstreamWriter;
+
+    #[cfg(feature = "nats-tests")]
+    #[tokio::test]
+    async fn test_jetstream_read() {
+        let js_url = "localhost:4222";
+        // Create JetStream context
+        let client = async_nats::connect(js_url).await.unwrap();
+        let context = jetstream::new(client);
+
+        let stream_name = "test_cancellation-2";
+        context
+            .get_or_create_stream(stream::Config {
+                name: stream_name.into(),
+                subjects: vec![stream_name.into()],
+                max_message_size: 1024,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        let _consumer = context
+            .create_consumer_on_stream(
+                consumer::Config {
+                    name: Some(stream_name.to_string()),
+                    ack_policy: consumer::AckPolicy::Explicit,
+                    ..Default::default()
+                },
+                stream_name,
+            )
+            .await
+            .unwrap();
+
+        let buf_reader_config = BufferReaderConfig {
+            name: stream_name.to_string(),
+            partitions: 0,
+            streams: vec![],
+            batch_size: 2,
+            wip_ack_interval: Duration::from_millis(5),
+        };
+        let (js_reader_handle, mut js_reader_rx) = JetstreamReader::new(
+            stream_name.to_string(),
+            0,
+            context.clone(),
+            buf_reader_config,
+        )
+        .await
+        .unwrap();
+
+        let cancel_token = CancellationToken::new();
+        let writer = JetstreamWriter::new(
+            stream_name.to_string(),
+            0,
+            Default::default(),
+            context.clone(),
+            500,
+            cancel_token.clone(),
+        );
+
+        for i in 0..10 {
+            let message = Message {
+                keys: vec![format!("key_{}", i)],
+                value: format!("message {}", i).as_bytes().to_vec(),
+                offset: None,
+                event_time: Utc::now(),
+                id: MessageID {
+                    vertex_name: "vertex".to_string(),
+                    offset: format!("offset_{}", i),
+                    index: i,
+                },
+                headers: HashMap::new(),
+            };
+            let (success_tx, success_rx) = oneshot::channel::<Result<Offset>>();
+            let message_bytes: BytesMut = message.try_into().unwrap();
+            writer.write(message_bytes.into(), success_tx).await;
+            success_rx.await.unwrap().unwrap();
+        }
+        // Cancel the token to exit the retry loop
+        cancel_token.cancel();
+
+        let mut buffer = vec![];
+        for i in 0..10 {
+            let Some(val) = js_reader_rx.recv().await else {
+                break;
+            };
+            buffer.push(val);
+        }
+        assert_eq!(
+            buffer.len(),
+            10,
+            "Expected 10 messages from the Jestream reader"
+        );
+        js_reader_handle.cancel();
+
+        tokio::time::sleep(Duration::from_millis(50)).await;
+        assert!(js_reader_rx.is_closed());
+
+        context.delete_stream(stream_name).await.unwrap();
     }
 }
