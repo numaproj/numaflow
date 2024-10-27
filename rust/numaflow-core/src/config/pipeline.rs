59afec976d2a680066358f3f5f4d55ef65964621
Yashash H L <yashashhl25@gmail.com>
2024-10-27 21:41:41 +0530

    fix test
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- rust/numaflow-core/src/config/pipeline.rs
+++ rust/numaflow-core/src/config/pipeline.rs
@@ -291,17 +291,13 @@ mod tests {
     #[test]
     fn test_default_pipeline_config() {
         let expected = PipelineConfig {
-            pipeline_name: "".to_string(),
-            vertex_name: "".to_string(),
+            pipeline_name: "default-pl".to_string(),
+            vertex_name: "default-vtx".to_string(),
             replica: 0,
-            batch_size: 0,
-            paf_batch_size: 0,
-            read_timeout: Duration::from_secs(0),
-            js_client_config: isb::jetstream::ClientConfig {
-                url: "".to_string(),
-                user: None,
-                password: None,
-            },
+            batch_size: DEFAULT_BATCH_SIZE as usize,
+            paf_batch_size: (DEFAULT_BATCH_SIZE * 2) as usize,
+            read_timeout: Duration::from_secs(DEFAULT_TIMEOUT_IN_MS as u64),
+            js_client_config: isb::jetstream::ClientConfig::default(),
             from_vertex_config: vec![],
             to_vertex_config: vec![],
             vertex_config: VertexType::Source(SourceVtxConfig {
