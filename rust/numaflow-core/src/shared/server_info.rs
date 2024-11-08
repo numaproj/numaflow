745307b634c5b709e9b32091443520d51ab6b547
Keran Yang <yangkr920208@gmail.com>
2024-11-08 15:43:29 -0500

    temp - fix ut
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- rust/numaflow-core/src/shared/server_info.rs
+++ rust/numaflow-core/src/shared/server_info.rs
@@ -142,7 +142,7 @@ fn check_sdk_compatibility(
 
             if !specifiers.contains(&sdk_version_pep440) {
                 return Err(Error::ServerInfo(format!(
-                    "SDK version {} must be upgraded to at least {}, in order to work with the current numaflow version",
+                    "Python SDK version {} must be upgraded to at least {}, in order to work with the current numaflow version",
                     sdk_version_pep440, human_readable(sdk_required_version)
                 )));
             }
@@ -373,10 +373,10 @@ mod version {
         java_version_map.insert("sinker".to_string(), "0.9.0-z".to_string());
         java_version_map.insert("fb-sinker".to_string(), "0.9.0-z".to_string());
         let mut rust_version_map = HashMap::new();
-        rust_version_map.insert("sourcer".to_string(), "0.2.0-z".to_string());
-        rust_version_map.insert("sourcetransformer".to_string(), "0.2.0-z".to_string());
-        rust_version_map.insert("sinker".to_string(), "0.2.0-z".to_string());
-        rust_version_map.insert("fb-sinker".to_string(), "0.2.0-z".to_string());
+        rust_version_map.insert("sourcer".to_string(), "0.1.1-z".to_string());
+        rust_version_map.insert("sourcetransformer".to_string(), "0.1.1-z".to_string());
+        rust_version_map.insert("sinker".to_string(), "0.1.1-z".to_string());
+        rust_version_map.insert("fb-sinker".to_string(), "0.1.1-z".to_string());
 
         let mut m = HashMap::new();
         m.insert("go".to_string(), go_version_map);
