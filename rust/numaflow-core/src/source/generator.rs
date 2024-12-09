08fc1e46791bdaaa36786311f32e1b491660eedf
Sreekanth <prsreekanth920@gmail.com>
2024-12-09 11:10:05 +0530

    Enable LTO optimization for release build
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/numaflow-core/src/source/generator.rs
+++ rust/numaflow-core/src/source/generator.rs
@@ -17,7 +17,7 @@ use crate::source;
 ///              =========================================================================> time
 ///  Read RPU=5: | :xxx:xx:  | :xxx <delay>             |:xxx:xx:| :xxx:xx:  | :xxx:xx:  |
 ///                2 batches   only 1 batch (no reread)      5         5           5
-///                 
+///
 /// ```
 /// NOTE: The minimum granularity of duration is 10ms.
 mod stream_generator {
@@ -350,7 +350,7 @@ impl source::SourceReader for GeneratorRead {
     }
 
     fn partitions(&self) -> Vec<u16> {
-        todo!()
+        unimplemented!()
     }
 }
 
--- rust/numaflow-models/src/lib.rs
+++ rust/numaflow-models/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::all)]
+
 #[macro_use]
 extern crate serde_derive;
 
