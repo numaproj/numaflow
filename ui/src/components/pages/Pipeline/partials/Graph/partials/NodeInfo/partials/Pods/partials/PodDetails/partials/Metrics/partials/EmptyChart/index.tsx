93be874dbbc29104261ec7996866953e3b31f4fd
adarsh0728 <gooneriitk@gmail.com>
2024-12-19 21:13:00 +0530

    chore: display err mssg based on api err response
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- ui/src/components/pages/Pipeline/partials/Graph/partials/NodeInfo/partials/Pods/partials/PodDetails/partials/Metrics/partials/EmptyChart/index.tsx
+++ ui/src/components/pages/Pipeline/partials/Graph/partials/NodeInfo/partials/Pods/partials/PodDetails/partials/Metrics/partials/EmptyChart/index.tsx
@@ -3,12 +3,12 @@ import CircleIcon from "@mui/icons-material/Circle";
 
 import "./style.css";
 
-const EmptyChart = () => {
+const EmptyChart = ({ message }: { message?: string }) => {
   return (
     <Box className={"empty_chart_container"}>
       <CircleIcon className={"circle_icon"} />
 
-      <Box className={"empty_text"}>No data for the selected filters.</Box>
+      <Box className={"empty_text"}>{message || "No data for the selected filters."}</Box>
     </Box>
   );
 };
