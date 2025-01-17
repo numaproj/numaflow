8e66fe06d0948b7ed2eb823be8862a8d15492b55
Adarsh Jain <gooneriitk@gmail.com>
2025-01-17 21:16:25 +0530

    chore: display error mssg based on api error response for metric charts (#2298)
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- ui/src/components/pages/Pipeline/partials/Graph/partials/NodeInfo/partials/Pods/partials/PodDetails/partials/Metrics/partials/EmptyChart/index.tsx
+++ ui/src/components/pages/Pipeline/partials/Graph/partials/NodeInfo/partials/Pods/partials/PodDetails/partials/Metrics/partials/EmptyChart/index.tsx
@@ -1,14 +1,16 @@
 import Box from "@mui/material/Box";
 import CircleIcon from "@mui/icons-material/Circle";
-
 import "./style.css";
+interface EmptyChartProps {
+  message?: string;
+}
 
-const EmptyChart = () => {
+const EmptyChart = ({ message }: EmptyChartProps) => {
   return (
     <Box className={"empty_chart_container"}>
       <CircleIcon className={"circle_icon"} />
 
-      <Box className={"empty_text"}>No data for the selected filters.</Box>
+      <Box className={"empty_text"}>{message ? "Error occurred while retrieving data" : "No data for the selected filters."}</Box>
     </Box>
   );
 };
