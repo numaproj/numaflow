0517d6c13a6c8a4c9385646cec9da976a79c1b14
Yashash H L <yashashhl25@gmail.com>
2025-02-11 06:44:14 +0530

    fix todo doc
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- rust/numaflow-core/src/watermark/idle.rs
+++ rust/numaflow-core/src/watermark/idle.rs
@@ -2,11 +2,14 @@
 //!
 //! > Read == 0
 //!
-//! TODO
+//! If no messages are being read from the source or ISB, we should be able to detect
+//! and publish the idle watermark.
 //!
 //! > Branch Idling
 //!
-//! TODO
+//! With conditional forwarding, some streams(branches) will get less frequent writes
+//! than others. In such cases, we should be able to detect and publish the idle
+//! watermark to those branches.
 
 /// Idle detection and handling for ISB.
 pub(super) mod isb;
