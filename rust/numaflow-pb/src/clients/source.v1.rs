0d08f4e7a209b3f736273d2d40837eccb12ed769
Vigith Maurice <vigith@gmail.com>
2025-04-01 11:02:28 -0700

    chore: upgrade crates (#2510)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>
    Co-authored-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/numaflow-pb/src/clients/source.v1.rs
+++ rust/numaflow-pb/src/clients/source.v1.rs
@@ -297,7 +297,7 @@ pub mod source_client {
     }
     impl<T> SourceClient<T>
     where
-        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T: tonic::client::GrpcService<tonic::body::Body>,
         T::Error: Into<StdError>,
         T::ResponseBody: Body<Data = Bytes> + std::marker::Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + std::marker::Send,
@@ -318,13 +318,13 @@ pub mod source_client {
             F: tonic::service::Interceptor,
             T::ResponseBody: Default,
             T: tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
+                http::Request<tonic::body::Body>,
                 Response = http::Response<
-                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                    <T as tonic::client::GrpcService<tonic::body::Body>>::ResponseBody,
                 >,
             >,
             <T as tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
+                http::Request<tonic::body::Body>,
             >>::Error: Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             SourceClient::new(InterceptedService::new(inner, interceptor))
