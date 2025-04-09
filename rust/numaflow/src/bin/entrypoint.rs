1bec1ca2d2eadfe08024449694e8553738c90b34
Vigith Maurice <vigith@gmail.com>
2025-04-08 21:33:58 -0700

    feat: start golang vs. rust image based on env (#2537)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

---
+++ rust/numaflow/src/bin/entrypoint.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::error::Error;
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+
+const NUMAFLOW_GOLANG: &str = "/bin/numaflow";
+const NUMAFLOW_RUST: &str = "/bin/numaflow-rs";
+
+fn start_golang() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::new(NUMAFLOW_GOLANG);
+    Err(cmd.args(env::args()).envs(env::vars()).exec().into())
+}
+
+fn start_rust() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::new(NUMAFLOW_RUST);
+    Err(cmd.args(env::args()).envs(env::vars()).exec().into())
+}
+
+ fn main() -> Result<(), Box<dyn Error>> {
+     // check whether environment variable
+     let is_golang = env::var("NUMAFLOW_RUNTIME").unwrap_or("golang".to_string());
+     if is_golang == "golang" {
+         start_golang()
+     } else { 
+         start_rust()
+     }
+ }
