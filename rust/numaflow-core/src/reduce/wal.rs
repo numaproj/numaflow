47296bf6a4b20ea017b74ab4b948b81cf0ffa551
Vigith Maurice <vigith@gmail.com>
2025-04-22 10:47:25 -0700

    fix: compilation
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

--- rust/numaflow-core/src/reduce/wal.rs
+++ rust/numaflow-core/src/reduce/wal.rs
@@ -1,11 +1,12 @@
-use std::collections::HashMap;
-use std::sync::Arc;
-use chrono::Utc;
-use tokio_stream::wrappers::ReceiverStream;
-use numaflow_pb::objects::wal::GcEvent;
 use crate::message::{IntOffset, Message, MessageID, Offset};
 use crate::reduce::wal::segment::append::{AppendOnlyWal, FileWriterMessage};
 use crate::shared::grpc::prost_timestamp_from_utc;
+use bytes::Bytes;
+use chrono::Utc;
+use numaflow_pb::objects::wal::GcEvent;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio_stream::wrappers::ReceiverStream;
 
 /// A WAL Segment.
 pub(crate) mod segment;
@@ -18,16 +19,16 @@ pub(crate) mod error;
 
 // GC Event - (start, end, optional keys)
 //
-// GC Events WAL contract is that any GC event that is written means all the messages < the window 
+// GC Events WAL contract is that any GC event that is written means all the messages < the window
 // end time can be deleted from the segment WAL(if keys are present only the messages with same
 // keys will be deleted)
 //
 // Fixed window of 60s duration GC Events - (60, 120), (120, 180), (180, 240)
 // Sliding window of 60s length and slide 10s GC Events - (60, 70), (70, 80), (80, 90)
 // Session window with 10s timeout GC Events - (60, 100, [key1, key2]), (120, 1000, [key3])
-// 
 //
-// Compaction logic for Aligned kind 
+//
+// Compaction logic for Aligned kind
 // 1. Replay all the GC events and store the max end time
 // 2. Replay all the data events and only retain the messages with event time > max end time
 //
@@ -39,7 +40,15 @@ pub(crate) mod error;
 // during compaction we should make sure the order of the messages does not change.
 
 async fn simple_data_wal_writer() {
-    let data_wal = AppendOnlyWal::new("segment", "var/run/numaflow".into(), 20 * 1024 * 1024, 1000, 500).await.unwrap();
+    let data_wal = AppendOnlyWal::new(
+        "segment",
+        "var/run/numaflow".into(),
+        20 * 1024 * 1024,
+        1000,
+        500,
+    )
+    .await
+    .unwrap();
     let messages: Vec<Message> = (0..10)
         .map(|i| {
             let headers = HashMap::new();
@@ -63,27 +72,39 @@ async fn simple_data_wal_writer() {
         .collect();
 
     let (tx, rx) = tokio::sync::mpsc::channel(10);
-    let (offset_stream, handle) = data_wal.streaming_write(ReceiverStream::new(rx)).await.unwrap();
+    let (offset_stream, handle) = data_wal
+        .streaming_write(ReceiverStream::new(rx))
+        .await
+        .unwrap();
     // write messages
     for message in messages {
-        let _ = tx.send(FileWriterMessage::WriteData {
-            id: message.id.to_string(),
-            data: message.into(), // impl from for bytes
-        }).await;
+        let _ = tx
+            .send(FileWriterMessage::WriteData {
+                id: message.id.to_string(),
+                data: message.try_into().unwrap(), // impl from for bytes
+            })
+            .await;
     }
-    
-    let gc_wal = AppendOnlyWal::new("gc", "var/run/numaflow".into(), 1 * 1024 * 1024, 1000, 500).await.unwrap();
+
+    let gc_wal = AppendOnlyWal::new("gc", "var/run/numaflow".into(), 1 * 1024 * 1024, 1000, 500)
+        .await
+        .unwrap();
     let gc_event = GcEvent {
         start_time: Some(prost_timestamp_from_utc(Utc::now())),
         end_time: Some(prost_timestamp_from_utc(Utc::now())),
         keys: vec![],
     };
-    
+
     let (tx, rx) = tokio::sync::mpsc::channel(10);
-    let (offset_stream, handle) = gc_wal.streaming_write(ReceiverStream::new(rx)).await.unwrap();
+    let (offset_stream, handle) = gc_wal
+        .streaming_write(ReceiverStream::new(rx))
+        .await
+        .unwrap();
     // write gc event
-    let _ = tx.send(FileWriterMessage::WriteData {
-        id: "gc".to_string(),
-        data: gc_event.into(), // impl from for bytes
-    }).await;
+    let _ = tx
+        .send(FileWriterMessage::WriteData {
+            id: "gc".to_string(),
+            data: Bytes::from(prost::Message::encode_to_vec(&gc_event)), // impl from for bytes
+        })
+        .await;
 }
