f396869467d3b78ced4dfcc23267d082155911d4
Adarsh Jain <gooneriitk@gmail.com>
2025-05-16 14:27:42 +0530

    feat: exponential backoff retry strategy for sink (#2614)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -8,7 +8,7 @@ go install -mod=vendor ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen
 
 header "Generating CRDs"
 # maxDescLen=0 avoids `kubectl apply` failing due to annotations being too long
-$(go env GOPATH)/bin/controller-gen crd:crdVersions=v1,maxDescLen=0 paths=./pkg/apis/... output:dir=config/base/crds/full
+$(go env GOPATH)/bin/controller-gen crd:crdVersions=v1,allowDangerousTypes=true,maxDescLen=0 paths=./pkg/apis/... output:dir=config/base/crds/full
 
 cp config/base/crds/full/numaflow.numaproj.io*.yaml config/base/crds/minimal/
 
