64a757941782b48b292a88e2ebd43f8453500f9d
Yashash H L <yashashhl25@gmail.com>
2025-06-08 12:33:41 -0700

    chore: batch ack for sqs source
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- rust/numaflow-core/src/source/sqs.rs
+++ rust/numaflow-core/src/source/sqs.rs
@@ -275,13 +275,15 @@ pub mod tests {
     }
 
     fn get_delete_message_output() -> Rule {
-        let delete_message_output = mock!(aws_sdk_sqs::Client::delete_message)
+        let delete_message_output = mock!(aws_sdk_sqs::Client::delete_message_batch)
             .match_requests(|inp| {
-                inp.queue_url().unwrap() == "https://sqs.us-west-2.amazonaws.com/926113353675/test-q/"
-                    && inp.receipt_handle().unwrap() == "AQEBaZ+j5qUoOAoxlmrCQPkBm9njMWXqemmIG6shMHCO6fV20JrQYg/AiZ8JELwLwOu5U61W+aIX5Qzu7GGofxJuvzymr4Ph53RiR0mudj4InLSgpSspYeTRDteBye5tV/txbZDdNZxsi+qqZA9xPnmMscKQqF6pGhnGIKrnkYGl45Nl6GPIZv62LrIRb6mSqOn1fn0yqrvmWuuY3w2UzQbaYunJWGxpzZze21EOBtywknU3Je/g7G9is+c6K9hGniddzhLkK1tHzZKjejOU4jokaiB4nmi0dF3JqLzDsQuPF0Gi8qffhEvw56nl8QCbluSJScFhJYvoagGnDbwOnd9z50L239qtFIgETdpKyirlWwl/NGjWJ45dqWpiW3d2Ws7q"
+                inp.queue_url().unwrap()
+                    == "https://sqs.us-west-2.amazonaws.com/926113353675/test-q/"
             })
             .then_output(|| {
-                aws_sdk_sqs::operation::delete_message::DeleteMessageOutput::builder().build()
+                aws_sdk_sqs::operation::delete_message_batch::DeleteMessageBatchOutput::builder()
+                    .build()
+                    .unwrap()
             });
         delete_message_output
     }
