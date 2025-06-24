6a62d8e7a25eb23b6567ab39fd5b1213ce40ddde
Sreekanth <prsreekanth920@gmail.com>
2025-06-24 20:46:09 +0530

    feat: HTTP Basic auth support for Pulsar source/sink (#2749)
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/numaflow-models/src/models/mod.rs
+++ rust/numaflow-models/src/models/mod.rs
@@ -136,6 +136,8 @@ pub mod probe;
 pub use self::probe::Probe;
 pub mod pulsar_auth;
 pub use self::pulsar_auth::PulsarAuth;
+pub mod pulsar_basic_auth;
+pub use self::pulsar_basic_auth::PulsarBasicAuth;
 pub mod pulsar_sink;
 pub use self::pulsar_sink::PulsarSink;
 pub mod pulsar_source;
