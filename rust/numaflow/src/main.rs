9179ce67f432080a17a2b6849cfff87749d387e0
Sreekanth <prsreekanth920@gmail.com>
2025-07-03 12:12:54 +0530

    Tokio with single worker
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/numaflow/src/main.rs
+++ rust/numaflow/src/main.rs
@@ -5,34 +5,39 @@ use tracing::{error, info};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    info!("Starting numaflow");
-    // Set up the tracing subscriber. RUST_LOG can be used to set the log level.
-    // The default log level is `info`. The `axum::rejection=trace` enables showing
-    // rejections from built-in extractors at `TRACE` level.
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                // TODO: add a better default based on entry point invocation
-                //  e.g., serving/monovertex might need a different default
-                .unwrap_or_else(|_| "info".into()),
-        )
-        .with(tracing_subscriber::fmt::layer().with_ansi(false))
-        .init();
+fn main() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
 
-    // Setup the CryptoProvider (controls core cryptography used by rustls) for the process
-    rustls::crypto::aws_lc_rs::default_provider()
-        .install_default()
-        .expect("Installing default CryptoProvider");
+    rt.block_on(async {
+        // Set up the tracing subscriber. RUST_LOG can be used to set the log level.
+        // The default log level is `info`. The `axum::rejection=trace` enables showing
+        // rejections from built-in extractors at `TRACE` level.
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::EnvFilter::try_from_default_env()
+                    // TODO: add a better default based on entry point invocation
+                    //  e.g., serving/monovertex might need a different default
+                    .unwrap_or_else(|_| "info".into()),
+            )
+            .with(tracing_subscriber::fmt::layer().with_ansi(false))
+            .init();
 
-    if let Err(e) = run().await {
-        error!("{e:?}");
-        return Err(e);
-    }
-    info!("Exiting...");
+        // Setup the CryptoProvider (controls core cryptography used by rustls) for the process
+        rustls::crypto::aws_lc_rs::default_provider()
+            .install_default()
+            .expect("Installing default CryptoProvider");
 
-    Ok(())
+        if let Err(e) = run().await {
+            error!("{e:?}");
+            std::process::exit(1);
+        }
+
+        info!("Exited.");
+    })
 }
 
 async fn run() -> Result<(), Box<dyn Error>> {
