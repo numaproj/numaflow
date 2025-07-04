8168b0d46d631111864afcd903173edf1a1377b9
Vigith Maurice <vigith@gmail.com>
2025-07-04 09:17:10 -0700

    feat: add clap and a minimal cli builder
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

---
+++ rust/numaflow/src/cmdline.rs
@@ -0,0 +1,29 @@
+use clap::Command;
+
+pub(super) fn root_cli() -> Command {
+    Command::new("numaflow")
+        .author("Numaflow Authors")
+        .about("Numaflow is a stream processing framework")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(add_monitor_subcommand())
+        .subcommand(add_serving_subcommand())
+}
+
+fn add_monitor_subcommand() -> Command {
+    Command::new("monitor").about("Monitor exposes monitoring endpoints")
+}
+
+fn add_serving_subcommand() -> Command {
+    Command::new("serving").about("Serving System for Numaflow")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        root_cli().debug_assert();
+    }
+}
