79156da4a085c08e8e53f1caa132225813634229
Vigith Maurice <vigith@gmail.com>
2025-07-08 09:32:38 -0700

    fix: cli opts and clap (#2788)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>
    Co-authored-by: adarsh0728 <gooneriitk@gmail.com>

--- rust/numaflow/src/bin/entrypoint.rs
+++ rust/numaflow/src/bin/entrypoint.rs
@@ -10,16 +10,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Determine the runtime based on the NUMAFLOW_RUNTIME environment variable
     let runtime = env::var("NUMAFLOW_RUNTIME").unwrap_or_else(|_| "golang".to_string());
 
-    // Collect arguments, skipping the first one (binary name) because we will replace the process
-    // with the chosen binary.
-    let args: Vec<String> = env::args().skip(1).collect();
-
     // Choose the appropriate binary to execute
     let binary = if runtime == "rust" {
         NUMAFLOW_RUST
     } else {
         NUMAFLOW_GOLANG
     };
+
+    // Collect arguments, skipping the first one (binary name) because we will replace the process
+    // with the chosen binary.
+    let args: Vec<String> = env::args().skip(1).collect();
 
     // Replace the current process with the chosen binary
     let mut cmd = Command::new(binary);
