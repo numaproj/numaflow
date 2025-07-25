79edb890704ec8f4e824ee97486d9fa2c8fe7d4f
Sreekanth <prsreekanth920@gmail.com>
2025-07-25 12:26:02 +0530

    Formatting
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/extns/numaflow-nats/src/jetstream.rs
+++ rust/extns/numaflow-nats/src/jetstream.rs
@@ -1,5 +1,5 @@
-use std::time::{Duration, SystemTime};
 use std::collections::HashMap;
+use std::time::{Duration, SystemTime};
 
 use async_nats::jetstream::consumer::{AckPolicy, DeliverPolicy};
 use async_nats::jetstream::{AckKind, Message as JetstreamMessage, consumer};
@@ -22,16 +22,19 @@ use crate::{Error, NatsAuth, Result, TlsConfig, tls};
 pub struct ConsumerDeliverPolicy(DeliverPolicy);
 
 impl ConsumerDeliverPolicy {
+    // When using, assoicated constant (`ConsumerDeliverPolicy::ALL`) feels better compared to `Default::default()`
     pub const ALL: ConsumerDeliverPolicy = ConsumerDeliverPolicy(DeliverPolicy::All);
 }
 
 impl TryFrom<&str> for ConsumerDeliverPolicy {
     type Error = Error;
 
-    fn try_from(value: &str)-> Result<Self> {
-
-        let fields = value.split_ascii_whitespace().map(|v| v.trim().to_lowercase()).collect::<Vec<String>>();
-        let fields = fields.iter().map(|s|s.as_ref()).collect::<Vec<&str>>();
+    fn try_from(value: &str) -> Result<Self> {
+        let fields = value
+            .split_ascii_whitespace()
+            .map(|v| v.trim().to_lowercase())
+            .collect::<Vec<String>>();
+        let fields = fields.iter().map(|s| s.as_ref()).collect::<Vec<&str>>();
 
         let policy = match fields[..] {
             ["all"] => Self(DeliverPolicy::All),
@@ -39,26 +42,40 @@ impl TryFrom<&str> for ConsumerDeliverPolicy {
             ["last_per_subject"] => Self(DeliverPolicy::LastPerSubject),
             ["new"] => Self(DeliverPolicy::New),
             ["by_start_sequence", sequence] => {
-                let sequence_id = sequence.parse::<u64>().map_err(|err| Error::Other(format!("start_sequence '{sequence}' is not a valid unsigned integer: {err:?}")))?;
+                let sequence_id = sequence.parse::<u64>().map_err(|err| {
+                    Error::Other(format!(
+                        "start_sequence '{sequence}' is not a valid unsigned integer: {err:?}"
+                    ))
+                })?;
                 Self(DeliverPolicy::ByStartSequence {
                     start_sequence: sequence_id,
                 })
-            },
+            }
             ["by_start_time", start_time] => {
-                let epoch_ms = start_time.parse::<i64>().map_err(|err| Error::Other(format!("epoch time should be in milliseconds specified as a valid integer: {err:?}")))?;
+                let epoch_ms = start_time.parse::<i64>().map_err(|err| {
+                    Error::Other(format!(
+                        "epoch time should be in milliseconds specified as a valid integer: {err:?}"
+                    ))
+                })?;
                 let duration = Duration::from_millis(epoch_ms.unsigned_abs());
                 let timestamp = match epoch_ms.is_positive() {
                     true => SystemTime::UNIX_EPOCH.checked_add(duration),
                     false => SystemTime::UNIX_EPOCH.checked_sub(duration),
                 };
                 let Some(timestamp) = timestamp else {
-                    return Err(Error::Other(format!("Specified epoch_time '{start_time}' is out of bounds to be represented as systemt time")))
+                    return Err(Error::Other(format!(
+                        "Specified epoch_time '{start_time}' is out of bounds to be represented as systemt time"
+                    )));
                 };
                 Self(DeliverPolicy::ByStartTime {
                     start_time: timestamp.into(),
                 })
             }
-            _ => return Err(Error::Other(format!("Invalid option for DeliverPolicy: {value}"))),
+            _ => {
+                return Err(Error::Other(format!(
+                    "Invalid option for DeliverPolicy: {value}"
+                )));
+            }
         };
         Ok(policy)
     }
