15dd58903eda81a1f9af9d1abc5bbf60c38209e1
Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>
2025-10-07 12:01:37 -0400

    Fix lintint
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>

--- rust/numaflow-core/src/source/sqs.rs
+++ rust/numaflow-core/src/source/sqs.rs
@@ -232,7 +232,8 @@ pub mod tests {
         .unwrap();
 
         // create the forwarder with the source and sink writer
-        let forwarder = crate::monovertex::forwarder::Forwarder::new(source.clone(), None, sink_writer);
+        let forwarder =
+            crate::monovertex::forwarder::Forwarder::new(source.clone(), None, sink_writer);
 
         let cancel_token = cln_token.clone();
         let _forwarder_handle: JoinHandle<crate::error::Result<()>> = tokio::spawn(async move {
