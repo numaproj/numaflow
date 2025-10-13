4fc2af4b1f852041239f2c7d58e87326275f6530
Adarsh Jain <gooneriitk@gmail.com>
2025-10-13 11:34:28 +0530

    feat(UI): add fallback sink container in MonoVertex graph  (#3004)
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- ui/src/components/pages/Pipeline/partials/Graph/partials/CustomNode/index.tsx
+++ ui/src/components/pages/Pipeline/partials/Graph/partials/CustomNode/index.tsx
@@ -15,6 +15,7 @@ import sink from "../../../../../../../images/sink.png";
 import monoVertex from "../../../../../../../images/monoVertex.svg";
 import transformer from "../../../../../../../images/transformer.svg";
 import udf from "../../../../../../../images/map.png";
+import fallback from "../../../../../../../images/fallback.png";
 import input0 from "../../../../../../../images/input0.svg";
 import input1 from "../../../../../../../images/input1.svg";
 import input2 from "../../../../../../../images/input2.svg";
@@ -370,7 +371,7 @@ const CustomNode: FC<NodeProps> = ({
               <Tooltip
                 title={<Box className={"node-tooltip"}>Sink Container</Box>}
                 arrow
-                placement={"right"}
+                placement={data?.nodeInfo?.sink?.fallback ? "bottom" : "right"}
               >
                 <Box className={"mono-vertex-img-wrapper"}>
                   <img
@@ -380,6 +381,24 @@ const CustomNode: FC<NodeProps> = ({
                   />
                 </Box>
               </Tooltip>
+              {data?.nodeInfo?.sink?.fallback && arrowSvg}
+              {data?.nodeInfo?.sink?.fallback && (
+                <Tooltip
+                  title={
+                    <Box className={"node-tooltip"}>Fallback Sink Container</Box>
+                  }
+                  arrow
+                  placement={"right"}
+                >
+                  <Box className={"mono-vertex-img-wrapper"}>
+                    <img
+                      className={"mono-vertex-img"}
+                      src={fallback}
+                      alt={"fallback-sink-container"}
+                    />
+                  </Box>
+                </Tooltip>
+              )}
             </Box>
           </>
         )}
