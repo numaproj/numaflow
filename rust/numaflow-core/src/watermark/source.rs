85e58305daab5ad77219d39c61765a08b8bcfe2b
Yashash H L <yashashhl25@gmail.com>
2025-10-20 12:12:39 +0530

    update test
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- rust/numaflow-core/src/watermark/source.rs
+++ rust/numaflow-core/src/watermark/source.rs
@@ -1276,7 +1276,7 @@ mod tests {
             .await
             .expect("Failed to get ot bucket");
 
-        let timeout_duration = Duration::from_secs(1);
+        let timeout_duration = Duration::from_secs(2);
         let result = tokio::time::timeout(timeout_duration, async {
             loop {
                 if let Some(wmb) = ot_bucket.get("v1-0").await.expect("Failed to get wmb") {
@@ -1286,7 +1286,7 @@ mod tests {
                         return true; // Found idle watermark
                     }
                 }
-                tokio::time::sleep(Duration::from_millis(10)).await;
+                tokio::time::sleep(Duration::from_millis(50)).await;
             }
         })
         .await;
