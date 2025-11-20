4fe907330ad851010ca21c22b0dd294ba44f1f2d
Vigith Maurice <vigith@gmail.com>
2025-11-20 09:52:40 -0800

    fix: use std context (#3081)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

--- pkg/mvtxdaemon/server/service/rater/pod_tracker.go
+++ pkg/mvtxdaemon/server/service/rater/pod_tracker.go
@@ -17,6 +17,7 @@ limitations under the License.
 package rater
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net/http"
@@ -26,7 +27,6 @@ import (
 	"time"
 
 	"go.uber.org/zap"
-	"golang.org/x/net/context"
 
 	"github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
 	"github.com/numaproj/numaflow/pkg/shared/logging"
