5b4631b832c1cb0bee3c56f1335fd14a2162caa8
Derek Wang <whynowy@gmail.com>
2025-12-12 08:40:20 -0800

    chore: clean up managedFields from k8s objects from UI display (#3098)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -18,16 +18,12 @@ package routes
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/numaproj/numaflow/pkg/shared/logging"
 	v1 "github.com/numaproj/numaflow/server/apis/v1"
-	"github.com/numaproj/numaflow/server/authn"
 	"github.com/numaproj/numaflow/server/authz"
-	"github.com/numaproj/numaflow/server/common"
 )
 
 type SystemInfo struct {
@@ -75,6 +71,7 @@ func Routes(ctx context.Context, r *gin.Engine, sysInfo SystemInfo, authInfo Aut
 	// r1Group is a group of routes that require AuthN/AuthZ when auth is enabled.
 	// they share the AuthN/AuthZ middleware.
 	r1Group := r.Group(baseHref + "api/v1")
+	r1Group.Use(cleanResponseMiddleware())
 	if !authInfo.DisableAuth {
 		authorizer, err := authz.NewCasbinObject(ctx, authRouteMap)
 		if err != nil {
@@ -189,66 +186,4 @@ func v1Routes(ctx context.Context, r gin.IRouter, dexObj *v1.DexObject, localUse
 	r.POST("/metrics-proxy", handler.GetMetricData)
 	// Discover the metrics for a given object type.
 	r.GET("/metrics-discovery/object/:object", handler.DiscoverMetrics)
-}
-
-// authMiddleware is the middleware for AuthN/AuthZ.
-// it ensures the user is authenticated and authorized
-// to execute the requested action before sending the request to the api handler.
-func authMiddleware(ctx context.Context, authorizer authz.Authorizer, dexAuthenticator authn.Authenticator, localUsersAuthenticator authn.Authenticator, authRouteMap authz.RouteMap) gin.HandlerFunc {
-
-	return func(c *gin.Context) {
-
-		log := logging.FromContext(ctx)
-		var userInfo *authn.UserInfo
-
-		loginType, err := c.Cookie(common.LoginCookieName)
-		if err != nil {
-			errMsg := fmt.Sprintf("Failed to get login type: %v", err)
-			c.JSON(http.StatusUnauthorized, v1.NewNumaflowAPIResponse(&errMsg, nil))
-			c.Abort()
-			return
-		}
-
-		// Authenticate the user based on the login type.
-		if loginType == "dex" {
-			userInfo, err = dexAuthenticator.Authenticate(c)
-		} else if loginType == "local" {
-			userInfo, err = localUsersAuthenticator.Authenticate(c)
-		} else {
-			errMsg := fmt.Sprintf("unidentified login type received: %v", loginType)
-			c.JSON(http.StatusUnauthorized, v1.NewNumaflowAPIResponse(&errMsg, nil))
-			c.Abort()
-			return
-		}
-
-		if err != nil {
-			errMsg := fmt.Sprintf("Failed to authenticate user: %v", err)
-			c.JSON(http.StatusUnauthorized, v1.NewNumaflowAPIResponse(&errMsg, nil))
-			c.Abort()
-			return
-		}
-		// Check if the route requires authorization.
-		if authRouteMap.GetRouteFromContext(c) != nil && authRouteMap.GetRouteFromContext(c).RequiresAuthZ {
-			// Check if the user is authorized to execute the requested action.
-			isAuthorized := authorizer.Authorize(c, userInfo)
-			if isAuthorized {
-				// If the user is authorized, continue the request.
-				c.Next()
-			} else {
-				// If the user is not authorized, return an error.
-				errMsg := "user is not authorized to execute the requested action"
-				c.JSON(http.StatusForbidden, v1.NewNumaflowAPIResponse(&errMsg, nil))
-				c.Abort()
-			}
-		} else if authRouteMap.GetRouteFromContext(c) != nil && !authRouteMap.GetRouteFromContext(c).RequiresAuthZ {
-			// If the route does not require AuthZ, skip the AuthZ check.
-			c.Next()
-		} else {
-			// If the route is not present in the route map, return an error.
-			log.Errorw("route not present in routeMap", "route", authz.GetRouteMapKey(c))
-			errMsg := "Invalid route"
-			c.JSON(http.StatusForbidden, v1.NewNumaflowAPIResponse(&errMsg, nil))
-			c.Abort()
-		}
-	}
 }
