87f559ded39dd331a4c7026c760cf86c4f34e6ad
Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>
2025-12-16 09:51:40 -0500

    Fix config related glue code for bypass config
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>

--- rust/numaflow-core/src/config/monovertex.rs
+++ rust/numaflow-core/src/config/monovertex.rs
@@ -21,7 +21,7 @@ use crate::config::pipeline::map::MapVtxConfig;
 use crate::error::Error;
 use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
-use numaflow_models::models::{ForwardConditions, MonoVertex, MonoVertexSinkerCondition};
+use numaflow_models::models::{ForwardConditions, MonoVertex, MonoVertexBypassCondition};
 use serde_json::from_slice;
 
 const DEFAULT_BATCH_SIZE: u64 = 500;
@@ -38,9 +38,8 @@ pub(crate) struct MonovertexConfig {
     pub(crate) graceful_shutdown_time: Duration,
     pub(crate) replica: u16,
     pub(crate) source_config: SourceConfig,
-    pub(crate) source_to_sink_condition: Option<ToSinkCondition>,
+    pub(crate) bypass_condition: Option<ToSinkCondition>,
     pub(crate) map_config: Option<MapVtxConfig>,
-    pub(crate) map_to_sink_condition: Option<ToSinkCondition>,
     pub(crate) sink_config: SinkConfig,
     pub(crate) transformer_config: Option<TransformerConfig>,
     pub(crate) fb_sink_config: Option<SinkConfig>,
@@ -62,16 +61,15 @@ impl Default for MonovertexConfig {
                 read_ahead: false,
                 source_type: SourceType::Generator(GeneratorConfig::default()),
             },
-            source_to_sink_condition: None,
             sink_config: SinkConfig {
                 sink_type: SinkType::Log(sink::LogConfig::default()),
                 retry_config: None,
             },
             map_config: None,
-            map_to_sink_condition: None,
             transformer_config: None,
             fb_sink_config: None,
             on_success_sink_config: None,
+            bypass_condition: None,
             metrics_config: MetricsConfig::default(),
             callback_config: None,
             rate_limit: None,
@@ -130,11 +128,7 @@ impl MonovertexConfig {
             .clone()
             .ok_or_else(|| Error::Config("Source not found".to_string()))?;
 
-        let source_to_sink_conditional = mono_vertex_obj
-            .spec
-            .forwarding_rules
-            .clone()
-            .and_then(|forwarding_rules| forwarding_rules.source);
+        let bypass_condition = mono_vertex_obj.spec.bypass;
 
         let source = SourceSpec::new(mono_vertex_name.clone(), "mvtx".into(), source);
         let source_type: SourceType = source.try_into()?;
@@ -177,12 +171,6 @@ impl MonovertexConfig {
             .udf
             .clone()
             .ok_or_else(|| Error::Config("Map UDF not found".to_string()));
-
-        let map_to_sink_conditional = mono_vertex_obj
-            .spec
-            .forwarding_rules
-            .clone()
-            .and_then(|forwarding_rules| forwarding_rules.udf);
 
         let map_config = match udf {
             Ok(udf) => Some(MapVtxConfig {
@@ -251,10 +239,7 @@ impl MonovertexConfig {
             read_timeout: Duration::from_millis(timeout_in_ms as u64),
             graceful_shutdown_time: Duration::from_secs(graceful_shutdown_time_secs),
             metrics_config: MetricsConfig::with_lookback_window_in_secs(look_back_window),
-            source_to_sink_condition: source_to_sink_conditional
-                .and_then(|condition| condition.try_into().ok()),
-            map_to_sink_condition: map_to_sink_conditional
-                .and_then(|condition| condition.try_into().ok()),
+            bypass_condition: bypass_condition.and_then(|condition| condition.try_into().ok()),
             source_config,
             map_config,
             sink_config,
@@ -274,9 +259,9 @@ pub(crate) struct ToSinkCondition {
     pub(crate) on_success: Option<Box<ForwardConditions>>,
 }
 
-impl TryFrom<Box<MonoVertexSinkerCondition>> for ToSinkCondition {
+impl TryFrom<Box<MonoVertexBypassCondition>> for ToSinkCondition {
     type Error = Error;
-    fn try_from(mvtx_sinker_condition: Box<MonoVertexSinkerCondition>) -> Result<Self> {
+    fn try_from(mvtx_sinker_condition: Box<MonoVertexBypassCondition>) -> Result<Self> {
         Ok(ToSinkCondition {
             sink: mvtx_sinker_condition.sink,
             fallback: mvtx_sinker_condition.fallback,
