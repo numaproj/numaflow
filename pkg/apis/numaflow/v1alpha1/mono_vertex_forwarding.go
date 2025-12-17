1de6f1d11889e51f5bd1b9dc8618e5728c1554cb
Vaibhav Kant Tiwari <vaibhav.tiwari33@gmail.com>
2025-12-17 17:24:58 -0500

    feat: Spec changes for short-circuiting mvtx (#3105)
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>

---
+++ pkg/apis/numaflow/v1alpha1/mono_vertex_forwarding.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2022 The Numaproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+type MonoVertexBypassCondition struct {
+	// Conditions to trigger bypass to primary sink component.
+	// Definition is similar to "conditions" in pipeline spec for edges.
+	// +optional
+	Sink *ForwardConditions `json:"sink" protobuf:"bytes,1,opt,name=sink"`
+	// Conditions to trigger bypass to fallback sink component.
+	// Definition is similar to "conditions" in pipeline spec for edges.
+	// +optional
+	Fallback *ForwardConditions `json:"fallback" protobuf:"bytes,2,opt,name=fallback"`
+	// Conditions to trigger bypass to onSuccess sink component.
+	// Definition is similar to "conditions" in pipeline spec for edges.
+	// +optional
+	OnSuccess *ForwardConditions `json:"onSuccess" protobuf:"bytes,3,opt,name=onSuccess"`
+}
--- pkg/apis/numaflow/v1alpha1/mono_vertex_types.go
+++ pkg/apis/numaflow/v1alpha1/mono_vertex_types.go
@@ -514,6 +514,12 @@ type MonoVertexSpec struct {
 	// +kubebuilder:default={"desiredPhase": Running}
 	// +optional
 	Lifecycle MonoVertexLifecycle `json:"lifecycle,omitempty" protobuf:"bytes,14,opt,name=lifecycle"`
+	// Bypass defines the bypass destination and conditions to trigger bypass for the mono vertex components.
+	// If specified, the bypass will be triggered if the conditions are met at any of the components.
+	// The first level of the bypass spec specifies the destination to which the message will be forwarded to,
+	// and the next level specifies the conditions to trigger the said bypass.
+	// +optional
+	Bypass *MonoVertexBypassCondition `json:"bypass,omitempty" protobuf:"bytes,15,opt,name=bypass"`
 }
 
 func (mvspec MonoVertexSpec) DeepCopyWithoutReplicas() MonoVertexSpec {
