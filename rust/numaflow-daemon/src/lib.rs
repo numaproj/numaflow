1bb5de05b9471d35d41c9a86809a142e6cf24ac2
Keran Yang <yangkr920208@gmail.com>
2026-01-23 14:24:08 -0500

    dummy response
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- rust/numaflow-daemon/src/lib.rs
+++ rust/numaflow-daemon/src/lib.rs
@@ -20,7 +20,11 @@ impl MonoVertexDaemonService for MvtxDaemonService {
         &self,
         _: Request<()>,
     ) -> Result<Response<GetMonoVertexMetricsResponse>, Status> {
-        todo!();
+        println!("Keran: I received the call!");
+
+        let reply = GetMonoVertexMetricsResponse { metrics: None };
+
+        Ok(Response::new(reply))
     }
 
     async fn get_mono_vertex_status(
@@ -28,23 +32,31 @@ impl MonoVertexDaemonService for MvtxDaemonService {
         _: Request<()>,
     ) -> Result<Response<GetMonoVertexStatusResponse>, Status> {
         println!("Keran: I received the call!");
-        todo!();
+
+        let reply = GetMonoVertexStatusResponse { status: None };
+
+        Ok(Response::new(reply))
     }
 
     async fn get_mono_vertex_errors(
         &self,
         _: Request<GetMonoVertexErrorsRequest>,
     ) -> Result<Response<GetMonoVertexErrorsResponse>, Status> {
-        todo!();
+        println!("Keran: I received the call!");
+
+        let reply = GetMonoVertexErrorsResponse { errors: vec![] };
+
+        Ok(Response::new(reply))
     }
 }
 
-/// 4327
+/// Matches the DaemonServicePort in pkg/apis/numaflow/v1alpha1/const.go
+const DAEMON_SERVICE_PORT: u16 = 4327;
 
 pub async fn run_monovertex(mvtx_name: String) -> Result<(), Box<dyn Error>> {
     info!("MonoVertex name is {}", mvtx_name);
 
-    let addr = "[::]:4327".parse()?;
+    let addr = format!("[::]:{}", DAEMON_SERVICE_PORT).parse()?;
 
     let service = MvtxDaemonService::default();
 
