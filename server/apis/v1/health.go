9f9b9c6041a88645e6af8f82f73c78591e395661
Dillen Padhiar <38965141+dpadhiar@users.noreply.github.com>
2026-02-10 16:56:56 -0800

    feat: use initContainerStatuses in health checks (#3201)
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- server/apis/v1/health.go
+++ server/apis/v1/health.go
@@ -194,7 +194,7 @@ func isVertexHealthy(h *handler, ns string, pipeline string, vertex *dfv1.Vertex
 			Code:    "V6",
 		}, err
 	}
-	// Iterate over all the pods, and verify if all the containers in the pod are in running state
+	// Iterate over all the pods, and verify if all the containers and initContainers in the pod are in a healthy state
 	for _, pod := range pods.Items {
 		// Iterate over all the containers in the pod
 		for _, containerStatus := range pod.Status.ContainerStatuses {
@@ -207,6 +207,17 @@ func isVertexHealthy(h *handler, ns string, pipeline string, vertex *dfv1.Vertex
 				}, nil
 			}
 		}
+		// Iterate over all the initContainers in the pod
+		for _, containerStatus := range pod.Status.InitContainerStatuses {
+			// check that the container terminated successfully
+			if containerStatus.State.Terminated != nil && containerStatus.State.Terminated.Reason == "Error" {
+				return false, &resourceHealthResponse{
+					Message: fmt.Sprintf("Init container %q in pod %q is not healthy", containerStatus.Name, pod.Name),
+					Code:    "V3",
+				}, nil
+			}
+		}
+
 	}
 	return true, &resourceHealthResponse{
 		Message: fmt.Sprintf("Vertex %q is healthy", vertexName),
