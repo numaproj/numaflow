ce2ac174d117843196d69492d6548c460970446e
Vaibhav Kant Tiwari <vaibhav.tiwari33@gmail.com>
2026-04-29 20:33:24 -0400

    fix: append parent index information in message ID offset (#3394)
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>
    Co-authored-by: Sreekanth <prsreekanth920@gmail.com>

--- rust/numaflow-core/src/mapper/map.rs
+++ rust/numaflow-core/src/mapper/map.rs
@@ -441,8 +441,9 @@ pub(crate) struct ParentMessageInfo {
     pub(crate) is_late: bool,
     pub(crate) headers: Arc<HashMap<String, String>>,
     pub(crate) start_time: Instant,
-    /// this remains 0 for all except map-streaming because in map-streaming there could be more than
-    /// one response for a single request.
+    /// Used to disambiguate sibling messages that share the same offset.
+    /// Also used for propagating this index as part of offset to maintain
+    /// uniqueness across transformer-map combination (e.g. fan-out from a source transformer).
     pub(crate) current_index: i32,
     pub(crate) metadata: Option<Arc<Metadata>>,
 }
@@ -473,7 +474,7 @@ impl From<Message> for MapRequest {
                 headers: Arc::unwrap_or_clone(message.headers),
                 metadata: message.metadata.map(|m| Arc::unwrap_or_clone(m).into()),
             }),
-            id: message.offset.to_string(),
+            id: message.id.to_string(),
             handshake: None,
             status: None,
         }
@@ -490,7 +491,7 @@ impl From<UserDefinedMessage<'_>> for Message {
             id: MessageID {
                 vertex_name: get_vertex_name().to_string().into(),
                 index: value.2,
-                offset: value.1.offset.to_string().into(),
+                offset: format!("{}-{}", value.1.offset, value.1.current_index).into(),
             },
             keys: Arc::from(value.0.keys),
             tags: Some(Arc::from(value.0.tags)),
