2a739f7b525bdec9b9e796e879a4ad53e7805656
Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>
2026-05-09 07:57:34 -0400

    info logs to track how many messages we received and set at map stream
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>

--- rust/numaflow-core/src/mapper/map/stream.rs
+++ rust/numaflow-core/src/mapper/map/stream.rs
@@ -6,19 +6,19 @@ use crate::config::is_mono_vertex;
 use crate::error::{Error, Result};
 use crate::message::{Message, MessageHandle};
 use crate::{mark_failed, mark_success};
-use numaflow_pb::clients::map::{self, MapRequest, MapResponse, map_client::MapClient};
-use tokio::sync::{OwnedSemaphorePermit, mpsc};
+use numaflow_pb::clients::map::{self, map_client::MapClient, MapRequest, MapResponse};
+use tokio::sync::{mpsc, OwnedSemaphorePermit};
 use tokio_stream::StreamExt;
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::AbortOnDropHandle;
-use tonic::Streaming;
 use tonic::transport::Channel;
-use tracing::{error, warn};
+use tonic::Streaming;
+use tracing::{error, info, warn};
 
 use super::{
-    ParentMessageInfo, STREAMING_MAP_RESP_CHANNEL_SIZE, SharedMapTaskContext, UserDefinedMessage,
-    create_response_stream, update_udf_error_metric, update_udf_process_time_metric,
-    update_udf_read_metric, update_udf_write_only_metric,
+    create_response_stream, update_udf_error_metric, update_udf_process_time_metric, update_udf_read_metric,
+    update_udf_write_only_metric, ParentMessageInfo, SharedMapTaskContext,
+    UserDefinedMessage, STREAMING_MAP_RESP_CHANNEL_SIZE,
 };
 
 /// Type alias for the stream response - raw results from the UDF
@@ -88,6 +88,8 @@ impl MapStreamTask {
             .await
             .expect("failed to reset tracker");
 
+        info!(?self.msg_handle.message.id, "Received message id");
+
         loop {
             tokio::select! {
                 result = receiver.recv() => {
@@ -136,6 +138,8 @@ impl MapStreamTask {
                                         } else {
                                             msg_handle
                                         };
+
+                                        info!(?msg_handle.message.id, "Sending message id");
 
                                         self.shared_ctx
                                             .output_tx
